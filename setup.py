b8ba0fc22a3fb50f27bd57c21c543f89f5be9f07
Haifeng <jhfjhfj1@gmail.com>
2017-11-20 16:02:16 -0600

    setup

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name='autokeras',
     packages=['autokeras'],  # this must be the same as the name above
     requires=['pytest', 'numpy'],
-    version='alpha v0.1',
+    version='v0.1-alpha',
     description='Automated Machine Learning with Keras',
     author='Haifeng Jin',
     author_email='jhfjhfj1@gmail.com',
     url='https://github.com/jhfjhfj1/auto-keras',  # use the URL to the github repo
-    download_url='https://github.com/jhfjhfj1/auto-keras/archive/0.1.tar.gz',  # I'll explain this in a second
+    download_url='https://github.com/jhfjhfj1/autokeras/archive/v0.1-alpha.tar.gz',  # I'll explain this in a second
     keywords=['automl'],  # arbitrary keywords
     classifiers=[]
 )
