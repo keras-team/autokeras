9b399de7f2904111df3a72c34308fa8c9287d70d
Haifeng <jhfjhfj1@gmail.com>
2017-12-22 16:49:47 -0600

    release

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='autokeras',
     packages=['autokeras'],  # this must be the same as the name above
-    requires=['pytest', 'numpy', 'keras', 'scikit-learn', 'tensorflow'],
+    install_requires=['pytest', 'numpy', 'keras', 'scikit-learn', 'tensorflow'],
     version='0.0.2',
     description='Automated Machine Learning with Keras',
     author='Haifeng Jin',
     author_email='jhfjhfj1@gmail.com',
-    url='https://github.com/jhfjhfj1/auto-keras',
-    download_url='https://github.com/jhfjhfj1/autokeras/archive/0.0.1.tar.gz',
+    url='https://github.com/jhfjhfj1/autokeras',
+    download_url='https://github.com/jhfjhfj1/autokeras/archive/0.0.2.tar.gz',
     keywords=['automl'],  # arbitrary keywords
     classifiers=[]
 )
