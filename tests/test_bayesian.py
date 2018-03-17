35d9ddfc64522224b50b0cd0ac9a5e19d1aba95e
Haifeng <jhfjhfj1@gmail.com>
2018-03-16 20:11:54 -0500

    edit_distance implemented

--- tests/test_bayesian.py
+++ tests/test_bayesian.py
@@ -6,7 +6,7 @@ from tests.common import get_add_skip_model, get_concat_skip_model
 def test_edit_distance():
     descriptor1 = Graph(get_add_skip_model()).extract_descriptor()
     descriptor2 = Graph(get_concat_skip_model()).extract_descriptor()
-    assert edit_distance(descriptor1, descriptor2) == 3
+    assert edit_distance(descriptor1, descriptor2) == 2.0
 
 
 def test_gpr():
