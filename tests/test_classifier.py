b6c39ba7042f605a01f9c4834afb4f2f7432a6ca
amraw <amitrawat10@gmail.com>
2018-03-21 00:13:15 -0500

    Restoring path variables in test to there old value

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -49,7 +49,7 @@ def test_fit_predict(_):
     constant.MAX_MODEL_NUM = 2
     constant.EPOCHS_EACH = 1
     constant.N_NEIGHBORS = 1
-    path = '/home/amraw/d3m/autokeras/tests/resources/temp'
+    path = 'tests/resources/temp'
     clean_dir(path)
     clf = ImageClassifier(path=path, verbose=False)
     train_x = np.array([[[1], [2]], [[3], [4]]])
@@ -63,7 +63,7 @@ def test_fit_predict(_):
 @patch('multiprocessing.Process', new=MockProcess)
 @patch('autokeras.search.ModelTrainer.train_model', side_effect=lambda: None)
 def test_fit_predict2(_):
-    path = '/home/amraw/d3m/autokeras/tests/resources/temp'
+    path = 'tests/resources/temp'
     clean_dir(path)
     clf = ImageClassifier(path=path, verbose=False)
     constant.MAX_ITER_NUM = 1
@@ -89,7 +89,7 @@ def test_save_continue(_):
     train_x = np.random.rand(100, 25, 1)
     test_x = np.random.rand(100, 25, 1)
     train_y = np.random.randint(0, 5, 100)
-    path = '/home/amraw/d3m/autokeras/tests/resources/temp'
+    path = 'tests/resources/temp'
     clean_dir(path)
     clf = ImageClassifier(path=path, verbose=False)
     clf.n_epochs = 100
@@ -112,7 +112,7 @@ def test_fit_cvs_file_1(_):
     constant.MAX_MODEL_NUM = 2
     constant.EPOCHS_EACH = 1
     constant.N_NEIGHBORS = 1
-    path = '/home/amraw/d3m/autokeras/tests/resources'
+    path = 'tests/resources'
     clf = ImageClassifier(verbose=False, path=os.path.join(path, "temp"))
     clf.fit(cvs_file_path=os.path.join(path, "images_test/images_name.csv"),
             images_path=os.path.join(path, "images_test/same_image"))
