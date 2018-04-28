672d0b88a11ae52a0844f6656231ff57fc64d832
Haifeng Jin <jin@tamu.edu>
2018-04-28 00:04:26 -0500

    random_mnist

---
+++ experiments/random_mnist.py
@@ -0,0 +1,44 @@
+import sys
+from keras.datasets import mnist
+import numpy as np
+
+import os
+import GPUtil
+
+from autokeras.classifier import ImageClassifier
+
+
+def select_gpu():
+    try:
+        # Get the first available GPU
+        DEVICE_ID_LIST = GPUtil.getFirstAvailable()
+        DEVICE_ID = DEVICE_ID_LIST[0] # grab first element from list
+
+        # Set CUDA_VISIBLE_DEVICES to mask out all other GPUs than the first available device id
+        os.environ["CUDA_VISIBLE_DEVICES"] = str(DEVICE_ID)
+    except FileNotFoundError:
+        print("GPU not found")
+
+
+if __name__ == '__main__':
+    select_gpu()
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.reshape(x_train.shape + (1,))
+    x_test = x_test.reshape(x_test.shape + (1,))
+    print(x_test.shape)
+    X = np.concatenate((x_train, x_test))
+    Y = np.concatenate((y_train, y_test))
+    clf = ImageClassifier(searcher_type='random', path='/tmp/mnist_random/', verbose=False)
+
+    clf.fit(x_train, y_train, time_limit=12*60*60)
+    # clf.final_fit(x_train, y_train)
+    y = clf.evaluate(x_test, y_test)
+    print(y)
+    # MLP for Pima Indians Dataset with 10-fold cross validation
+    scores = clf.cross_validate(X, Y, 2)
+    print(scores)
+    print(np.mean(scores))
+    print(np.std(scores))
+
+    # split into input (X) and output (Y) variables
+    # define 10-fold cross validation test harness
