1e91fb9cb209050a46dc271939f05caaee5ff09a
Haifeng Jin <jin@tamu.edu>
2018-07-06 12:11:08 -0500

    release

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name='autokeras',
     packages=['autokeras'],  # this must be the same as the name above
     install_requires=['pytest', 'numpy', 'keras', 'scikit-learn', 'tensorflow', 'scipy'],
-    version='0.1.0',
+    version='0.1.1',
     description='Automated Machine Learning with Keras',
     author='Haifeng Jin',
     author_email='jhfjhfj1@gmail.com',
     url='http://autokeras.com',
-    download_url='https://github.com/jhfjhfj1/autokeras/archive/0.0.2.tar.gz',
+    download_url='https://github.com/jhfjhfj1/autokeras/archive/0.1.1.tar.gz',
     keywords=['automl'],  # arbitrary keywords
     classifiers=[]
 )
