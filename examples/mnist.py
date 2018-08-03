04ba53461970d0bc1ff7b552403791e0852cd739
Haifeng Jin <jin@tamu.edu>
2018-08-03 04:07:28 -0500

    example

--- examples/mnist.py
+++ examples/mnist.py
@@ -6,8 +6,6 @@ if __name__ == '__main__':
     x_train = x_train.reshape(x_train.shape + (1,))
     x_test = x_test.reshape(x_test.shape + (1,))
 
-    his = x_test.copy()
-
     clf = ImageClassifier(verbose=True, augment=False)
     clf.fit(x_train, y_train, time_limit=12 * 60 * 60)
     clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
