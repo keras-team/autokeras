fff8c5ae682789a27d1925d4af2895bc93fcd9e9
Haifeng Jin <jin@tamu.edu>
2018-08-03 02:38:26 -0500

    mnist example

--- examples/mnist.py
+++ examples/mnist.py
@@ -1,6 +1,5 @@
 from keras.datasets import mnist
 from autokeras.classifier import ImageClassifier
-from autokeras.constant import Constant
 
 if __name__ == '__main__':
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -9,12 +8,8 @@ if __name__ == '__main__':
 
     his = x_test.copy()
 
-    Constant.SEARCH_MAX_ITER = 1
-    Constant.MAX_ITER_NUM = 1
     clf = ImageClassifier(verbose=True, augment=False)
-    clf.fit(x_train, y_train, time_limit=30)
+    clf.fit(x_train, y_train, time_limit=12 * 60 * 60)
     clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
     y = clf.evaluate(x_test, y_test)
-    import numpy as np
-    assert False not in np.equal(his, x_test)
-    print(y)
+    print(y * 100)
