387bdbd489082282f45bce89b71565d335d4bfa5
ankapale <31904405+ankapale@users.noreply.github.com>
2018-08-10 17:28:11 -0500

    Add Base classifier (#69)
    * Added base class
    
    * Added base class

--- examples/mnist.py
+++ examples/mnist.py
@@ -1,5 +1,5 @@
 from keras.datasets import mnist
-from autokeras.classifier import ImageClassifier
+from autokeras.image_classifier import ImageClassifier
 
 if __name__ == '__main__':
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
