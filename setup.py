c3120c44981c418619246b7f375168b1451ee811
Haifeng Jin <jin@tamu.edu>
2018-08-17 11:35:13 -0500

    update to pytorch 0.4.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='autokeras',
     packages=['autokeras'],  # this must be the same as the name above
-    install_requires=['torch==0.4.0', 'torchvision==0.2.1', 'numpy==1.14.5', 'keras==2.2.2', 'scikit-learn==0.19.1',
+    install_requires=['torch==0.4.1', 'torchvision==0.2.1', 'numpy==1.14.5', 'keras==2.2.2', 'scikit-learn==0.19.1',
                       'tensorflow==1.8.0'],
     version='0.2.3',
     description='AutoML for deep learning',
