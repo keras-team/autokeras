386f8613eddebbf03f2dfa32d5591bcdf4510468
Haifeng Jin <jhfjhfj1@gmail.com>
2018-08-17 21:35:03 -0500

    [MRG] Clean up code (#97)
    * code inspection
    
    * add verbose test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,18 +4,18 @@ from autokeras.generator import CnnGenerator
 from autokeras.loss_function import classification_loss, regression_loss
 from autokeras.metric import Accuracy, MSE
 from autokeras.utils import ModelTrainer, temp_folder_generator
-from tests.common import get_classification_dataloaders, get_regression_dataloaders
+from tests.common import get_classification_data_loaders, get_regression_data_loaders
 
 
 def test_model_trainer_classification():
     model = CnnGenerator(3, (28, 28, 3)).generate().produce_model()
-    train_data, test_data = get_classification_dataloaders()
-    ModelTrainer(model, train_data, test_data, Accuracy, classification_loss, False).train_model(max_iter_num=3)
+    train_data, test_data = get_classification_data_loaders()
+    ModelTrainer(model, train_data, test_data, Accuracy, classification_loss, True).train_model(max_iter_num=3)
 
 
 def test_model_trainer_regression():
     model = CnnGenerator(1, (28, 28, 3)).generate().produce_model()
-    train_data, test_data = get_regression_dataloaders()
+    train_data, test_data = get_regression_data_loaders()
     ModelTrainer(model, train_data, test_data, MSE, regression_loss, False).train_model(max_iter_num=3)
 
 
