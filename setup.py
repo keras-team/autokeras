b6a9812a7f4c7b21aa11ce89f9d98664486d0f94
Asif Saifuddin Auvi <auvipy@gmail.com>
2018-08-27 00:57:04 +0600

    update numpy to 1.15.1 (#140)
    * update numpy to 1.15.1
    
    * updated dependencies

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='autokeras',
     packages=['autokeras'],  # this must be the same as the name above
-    install_requires=['torch==0.4.1', 'torchvision==0.2.1', 'numpy==1.14.5', 'keras==2.2.2', 'scikit-learn==0.19.1',
-                      'tensorflow==1.8.0'],
+    install_requires=['torch==0.4.1', 'torchvision==0.2.1', 'numpy>=1.15.1', 'keras==2.2.2', 'scikit-learn==0.19.1',
+                      'tensorflow>=1.10.1'],
     version='0.2.7',
     description='AutoML for deep learning',
     author='Haifeng Jin',
