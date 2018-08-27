43714528e84be709a11c8677aab10c180e8e69a6
song3134 <song_3134@tamu.edu>
2018-08-26 21:33:48 -0500

    Update setup.py (#147)

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
     name='autokeras',
     packages=['autokeras'],  # this must be the same as the name above
     install_requires=['torch==0.4.1', 'torchvision==0.2.1', 'numpy>=1.15.1', 'keras==2.2.2', 'scikit-learn==0.19.1',
-                      'tensorflow>=1.10.1'],
-    version='0.2.8',
+                      'tensorflow>=1.10.1', 'tqdm=4.25.0'],
+    version='0.2.9',
     description='AutoML for deep learning',
     author='Haifeng Jin',
     author_email='jhfjhfj1@gmail.com',
     url='http://autokeras.com',
-    download_url='https://github.com/jhfjhfj1/autokeras/archive/0.2.8.tar.gz',
+    download_url='https://github.com/jhfjhfj1/autokeras/archive/0.2.9.tar.gz',
     keywords=['automl'],  # arbitrary keywords
     classifiers=[]
 )
