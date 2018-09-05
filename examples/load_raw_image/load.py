88f987ac25ff7e5e6180d664a5dc8cd91e353eb2
Dieter Castel <dietercastel@gmail.com>
2018-09-05 16:33:17 +0200

    Fixed load.py and lifted it from the zip file. (#173)

---
+++ examples/load_raw_image/load.py
@@ -0,0 +1,31 @@
+#To run first unzip ../load_raw_image_data.zip into ../
+# so the train and test directories reside in this directory 
+
+from autokeras.image_supervised import load_image_dataset
+from keras.datasets import mnist
+from autokeras import ImageClassifier
+
+
+def load_images():
+    x_train, y_train = load_image_dataset(csv_file_path="train/label.csv",
+                                          images_path="train")
+    print(x_train.shape)
+    print(y_train.shape)
+
+    x_test, y_test = load_image_dataset(csv_file_path="test/label.csv",
+                                        images_path="test")
+    print(x_test.shape)
+    print(y_test.shape)
+    return x_train, y_train, x_test, y_test
+
+def run():
+    x_train, y_train, x_test, y_test = load_images()
+    #After loading train and evaluate classifier.
+    clf = ImageClassifier(verbose=True, augment=False)
+    clf.fit(x_train, y_train, time_limit=12 * 60 * 60)
+    clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
+    y = clf.evaluate(x_test, y_test)
+    print(y * 100)
+
+if __name__ == '__main__':
+    run()
