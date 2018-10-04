b6342c41c30daadb5741725e12ac6623994fc653
Haifeng Jin <jhfjhfj1@gmail.com>
2018-10-04 17:03:50 -0500

    0.2.18

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     packages=['autokeras'],  # this must be the same as the name above
     install_requires=['torch==0.4.1', 'torchvision==0.2.1', 'numpy>=1.14.5', 'keras==2.2.2', 'scikit-learn==0.19.1',
                       'tensorflow>=1.10.0', 'tqdm==4.25.0'],
-    version='0.2.17',
+    version='0.2.18',
     description='AutoML for deep learning',
     author='Haifeng Jin',
     author_email='jhfjhfj1@gmail.com',
     url='http://autokeras.com',
-    download_url='https://github.com/jhfjhfj1/autokeras/archive/0.2.17.tar.gz',
+    download_url='https://github.com/jhfjhfj1/autokeras/archive/0.2.18.tar.gz',
     keywords=['automl'],  # arbitrary keywords
     classifiers=[]
 )
