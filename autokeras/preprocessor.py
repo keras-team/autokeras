86f7a5807e5cb0b87fcde8e8cd1e90b8f2670bf8
Haifeng Jin <jhfjhfj1@gmail.com>
2018-10-18 15:06:11 -0500

    update

--- autokeras/preprocessor.py
+++ autokeras/preprocessor.py
@@ -112,3 +112,16 @@ class MultiTransformDataset(Dataset):
 
     def __len__(self):
         return len(self.dataset)
+
+
+# https://discuss.pytorch.org/t/loading-huge-data-functionality/346/2
+class MyDataset(torch.utils.Dataset):
+    def __init__(self):
+        self.data_files = os.listdir('data_dir')
+        sort(self.data_files)
+
+    def __getindex__(self, idx):
+        return load_file(self.data_files[idx])
+
+    def __len__(self):
+        return len(self.data_files)
