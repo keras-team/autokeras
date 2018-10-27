9d53685a5966b39674e44df9c6b9ce97c7f24b0a
Haifeng Jin <jhfjhfj1@gmail.com>
2018-10-26 21:46:58 -0500

    [WIP] Memory issue (#281)
    [MRG] Memory issue

--- tests/nn/test_generator.py
+++ tests/nn/test_generator.py
@@ -1,9 +1,13 @@
 from autokeras.nn.generator import *
 from autokeras.nn.graph import TorchModel
+import numpy as np
+import torch
 
 
 def test_default_generator():
     generator = CnnGenerator(3, (28, 28, 1))
     graph = generator.generate()
     model = graph.produce_model()
+    inputs = torch.Tensor(np.ones((100, 1, 28, 28)))
+    print(model(inputs).size())
     assert isinstance(model, TorchModel)
