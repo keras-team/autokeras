319ffa563ef1f25e6c886bf3a25085ad09f90446
droidadroit <yashwanth95tuniki@gmail.com>
2018-11-01 22:38:47 -0500

    Created MlpGenerator and a superclass

--- tests/nn/test_generator.py
+++ tests/nn/test_generator.py
@@ -2,8 +2,15 @@ from autokeras.nn.generator import *
 from autokeras.nn.graph import TorchModel
 
 
-def test_default_generator():
+def test_default_cnn_generator():
     generator = CnnGenerator(3, (28, 28, 1))
     graph = generator.generate()
+    model = graph.produce_model()
+    assert isinstance(model, TorchModel)
+
+
+def test_default_mlp_generator():
+    generator = MlpGenerator(5, (4,))
+    graph = generator.generate(3, [9, 8, 6])
     model = graph.produce_model()
     assert isinstance(model, TorchModel)
