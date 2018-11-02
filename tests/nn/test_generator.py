66086e58b376e25123f37cc9dba6181d9b927b04
droidadroit <yashwanth95tuniki@gmail.com>
2018-11-01 23:47:25 -0500

    Added tests

--- tests/nn/test_generator.py
+++ tests/nn/test_generator.py
@@ -15,6 +15,11 @@ def test_default_cnn_generator():
 
 def test_default_mlp_generator():
     generator = MlpGenerator(5, (4,))
+
     graph = generator.generate(3, [9, 8, 6])
+    model = graph.produce_model()
+    assert isinstance(model, TorchModel)
+
+    graph = generator.generate(3, 5)
     model = graph.produce_model()
     assert isinstance(model, TorchModel)
