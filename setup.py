4c190d0d01cc2a062e6c0eac87841944cf1c8ccc
Haifeng Jin <jhfjhfj1@gmail.com>
2018-11-16 20:19:44 -0600

    new release 0.3.0『』 『』

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
     packages=find_packages(exclude=('tests',)),
     install_requires=['torch==0.4.1', 'torchvision==0.2.1', 'numpy>=1.14.5', 'keras==2.2.2', 'scikit-learn==0.19.1',
                       'tensorflow==1.10.0', 'tqdm==4.25.0', 'imageio', 'GPUtil'],
-    version='0.2.19',
+    version='0.3.0',
     description='AutoML for deep learning',
     author='Haifeng Jin',
     author_email='jhfjhfj1@gmail.com',
     url='http://autokeras.com',
-    download_url='https://github.com/jhfjhfj1/autokeras/archive/0.2.19.tar.gz',
+    download_url='https://github.com/jhfjhfj1/autokeras/archive/0.3.0.tar.gz',
     keywords=['automl'],  # arbitrary keywords
     classifiers=[]
 )
