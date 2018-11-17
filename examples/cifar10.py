fa73ffdd8bc5ec4b95813ef4da526f1a2382eef9
Haifeng Jin <jhfjhfj1@gmail.com>
2018-11-16 20:47:37 -0600

    cifar

--- examples/cifar10.py
+++ examples/cifar10.py
@@ -1,15 +1,13 @@
 from keras.datasets import cifar10
 
 from autokeras import ImageClassifier
-from autokeras.nn.model_trainer import Constant
 
 if __name__ == '__main__':
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
     clf = ImageClassifier(verbose=True, augment=True)
-    Constant.MAX_NO_IMPROVEMENT_NUM = 33
-    clf.fit(x_train, y_train, time_limit=10 * 60)
+    clf.fit(x_train, y_train, time_limit=12*60*60)
     clf.final_fit(x_train, y_train, x_test, y_test, retrain=True,
-                  trainer_args={'max_iter_num': None, 'max_no_improvement_num': 630})
+                  trainer_args={'max_iter_num': 630, 'max_no_improvement_num': 630})
     y = clf.evaluate(x_test, y_test)
 
     print(y * 100)
