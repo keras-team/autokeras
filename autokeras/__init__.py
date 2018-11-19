ecaf056386237bbcfc08e0e6abda266b63795837
boyuangong <boyuangong@gmail.com>
2018-11-19 13:54:43 -0600

    add init

--- autokeras/__init__.py
+++ autokeras/__init__.py
@@ -1,2 +1,3 @@
 from autokeras.image.image_supervised import ImageClassifier, ImageRegressor
 from autokeras.text.text_supervised import TextClassifier, TextRegressor
+from autokeras.net_module import CnnGenerator, MlpModule
