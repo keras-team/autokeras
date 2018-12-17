597e18188e9cd3b8c141270471b3aa47e929e1d9
song3134 <song_3134@tamu.edu>
2018-12-16 22:17:42 -0600

    resolve conflicts in examples

--- examples/tabular_examples/tabular_classification_binary.py
+++ examples/tabular_examples/tabular_classification_binary.py
@@ -10,29 +10,16 @@ if __name__ == '__main__':
     x_mvc = np.random.randint(0, 10, [nsample, nmvc])
 
     x_all = np.concatenate([x_num, x_time, x_cat, x_mvc], axis=1)
-<<<<<<< HEAD
-    x_train = x_all[:int(nsample*0.8), :]
-    x_test = x_all[int(nsample * 0.8):, :]
-
-    y_all = np.random.randint(0, 2, nsample)
-    y_train = y_all[:int(nsample*0.8)]
-=======
     x_train = x_all[:int(nsample * 0.8), :]
     x_test = x_all[int(nsample * 0.8):, :]
 
     y_all = np.random.randint(0, 2, nsample)
     y_train = y_all[:int(nsample * 0.8)]
->>>>>>> 60a894217b158d958f47237ee81c13a3237a1146
     y_test = y_all[int(nsample * 0.8):]
 
     clf = TabularClassifier()
     datainfo = {'loaded_feat_types': [ntime, nnum, ncat, nmvc]}
-<<<<<<< HEAD
-    # clf.fit(x_train, y_train, time_limit=12 * 60 * 60, datainfo=None)
-    clf.fit(x_train, y_train, time_limit=12 * 60 * 60, datainfo=datainfo)
-=======
     clf.fit(x_train, y_train, time_limit=12 * 60 * 60, data_info=datainfo)
->>>>>>> 60a894217b158d958f47237ee81c13a3237a1146
 
     results = clf.evaluate(x_test, y_test)
     print(results)
