c2b51a759610d53ad1dfbe2d5f41a3f52acc45f8
droidadroit <yashwanth95tuniki@gmail.com>
2018-12-17 12:25:01 -0600

    handled case when output file is None

--- tests/test_pretrained.py
+++ tests/test_pretrained.py
@@ -9,8 +9,9 @@ def test_face_detection():
     if os.path.exists(out_file):
         os.remove(out_file)
     face_detection = FaceDetectionPretrained()
-    bboxs, landmarks = face_detection.predict(img_file, out_file)
+    bboxs1, landmarks1 = face_detection.predict(img_file, out_file)
     assert os.path.exists(out_file)
+    bboxs2, landmarks2 = face_detection.predict(img_file)
+    assert bboxs1.shape == bboxs2.shape == (11, 5) and landmarks1.shape == landmarks2.shape == (11, 10)
     os.remove(img_file)
     os.remove(out_file)
-    assert bboxs.shape == (11, 5) and landmarks.shape == (11, 10)
