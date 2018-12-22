0f9cf2b885583decddacf8e7e796af8f2273ba4f
Haifeng Jin <jhfjhfj1@gmail.com>
2018-12-21 22:43:51 -0600

    pretrain

--- tests/test_object_detection.py
+++ tests/test_object_detection.py
@@ -5,6 +5,7 @@ from tests.common import TEST_TEMP_DIR, clean_dir
 def test_object_detection():
     detector = ObjectDetector()
     detector.load()
-    result = detector.predict('/tests/resources/image_test/od.JPG', TEST_TEMP_DIR)
-    assert isinstance(result, tuple)
+    img_path = 'tests/resources/images_test/od.JPG'
+    result = detector.predict(img_path, TEST_TEMP_DIR)
+    assert isinstance(result, list)
     clean_dir(TEST_TEMP_DIR)
