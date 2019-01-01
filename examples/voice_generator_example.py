38330ab04e4f88f353c0057f437212afa6eb480c
boyuangong <boyuangong@gmail.com>
2019-01-01 01:09:45 -0600

    test

--- examples/voice_generator_example.py
+++
@@ -1,5 +0,0 @@
-from autokeras.pretrained.voice_generator.voice_generator import VoiceGenerator
-
-voice_generator = VoiceGenerator()
-text = "This is expensive, it costs me $300.2"
-voice_generator.generate(text, path="save.wav")
