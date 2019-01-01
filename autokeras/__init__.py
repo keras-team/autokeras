0681de15f67504ca7a3e410e42321bde94f79122
boyuangong <boyuangong@gmail.com>
2019-01-01 01:41:36 -0600

    remove test

--- autokeras/__init__.py
+++ autokeras/__init__.py
@@ -6,3 +6,4 @@ from autokeras.net_module import CnnModule, MlpModule
 
 from autokeras.pretrained.object_detector import ObjectDetector
 from autokeras.pretrained.face_detector import FaceDetector
+from autokeras.pretrained.voice_generator.voice_generator import VoiceGenerator
