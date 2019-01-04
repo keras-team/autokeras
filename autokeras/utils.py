2eeb71021566b24b7d9f6aa3c9125677e4a74a00
satyakesav <satyakesav123@gmail.com>
2019-01-04 11:30:53 -0800

    Pretrained text sentiment

--- autokeras/utils.py
+++ autokeras/utils.py
@@ -20,7 +20,6 @@ from autokeras.constant import Constant
 from scipy.ndimage import zoom
 
 
-
 class NoImprovementError(Exception):
     def __init__(self, message):
         self.message = message
@@ -305,3 +304,40 @@ def get_system():
         return Constant.SYS_WINDOWS
 
     raise EnvironmentError('Unsupported environment')
+
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+
+def save_response_content(response, destination):
+    chunk_size = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(chunk_size):
+            if chunk:
+                f.write(chunk)
+
+
+def download_file_from_google_drive(file_id, destination):
+    if not os.path.exists(destination):
+        print("Downloading file from Google Drive started.")
+
+        url = "https://docs.google.com/uc?export=download"
+
+        session = requests.Session()
+
+        response = session.get(url, params={'id': file_id}, stream=True)
+        token = get_confirm_token(response)
+
+        if token:
+            params = {'id': file_id, 'confirm': token}
+            response = session.get(url, params=params, stream=True)
+
+        save_response_content(response, destination)
+
+        print("Downloading file from Google Drive ended.")
---
+++ examples/senti.py
@@ -0,0 +1,5 @@
+from autokeras.pretrained.text_sentiment import TextSentiment
+
+text_cls = TextSentiment()
+
+text_cls.predict("The model is working well..")
