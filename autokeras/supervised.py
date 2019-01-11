db2a01b8e0df53bdbb6dd1ed6712a61a35ccf2d7
feedliu <feedliu15@gmail.com>
2019-01-11 16:48:10 +0800

    fix the order of y_true and y_pred parameters

--- autokeras/supervised.py
+++ autokeras/supervised.py
@@ -206,7 +206,7 @@ class DeepSupervised(Supervised):
     def evaluate(self, x_test, y_test):
         """Return the accuracy score between predict value and `y_test`."""
         y_predict = self.predict(x_test)
-        return self.metric().evaluate(y_test, y_predict)
+        return self.metric().evaluate(y_predict, y_test)
 
 
 class PortableClass(ABC):
@@ -332,4 +332,4 @@ class PortableDeepSupervised(PortableClass):
     def evaluate(self, x_test, y_test):
         """Return the accuracy score between predict value and `y_test`."""
         y_predict = self.predict(x_test)
-        return self.metric().evaluate(y_test, y_predict)
+        return self.metric().evaluate(y_predict, y_test)
