97c02dcd91d15ed9c5fb0cb805c65e6a6b2b5c77
Satya Kesav <satyakesav123@gmail.com>
2019-01-27 14:15:12 -0600

    [MRG] Sentiment analysis (#490)
    * sentiment_analysis
    
    * create mkdocs for sentiment analysis example
    
    * quick changes

--- examples/pretrained_models/text_models/sentiment_analysis_example.py
+++ examples/pretrained_models/text_models/sentiment_analysis_example.py
@@ -1,8 +1,8 @@
-from autokeras.pretrained.text_sentiment import TextSentiment
+from autokeras.pretrained.sentiment_analysis import SentimentAnalysis
 
-text_cls = TextSentiment()
+sentiment_cls = SentimentAnalysis()
 
-polarity = text_cls.predict("The model is working well..")
+polarity = sentiment_cls.predict("The model is working well..")
 
 print("Polarity of the input sentence is (sentiment is +ve if polarity > 0.5) : ", polarity)
 
