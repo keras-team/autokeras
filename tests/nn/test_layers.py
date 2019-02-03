8533ab66b179ac916b2f4d98267173d456c022af
Haifeng Jin <jhfjhfj1@gmail.com>
2019-02-03 08:12:56 -0600

    test1

--- tests/nn/test_layers.py
+++
@@ -1,8 +0,0 @@
-from autokeras.nn.layers import *
-import numpy as np
-
-
-def test_global_layer():
-    layer = GlobalAvgPool2d()
-    inputs = torch.Tensor(np.ones((100, 50, 30, 40)))
-    assert layer(inputs).size() == (100, 50)
