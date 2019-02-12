091199036e048b3003c19700d6658b404729b673
Satya Kesav <satya@Satyas-MacBook-Pro.local>
2019-02-11 22:26:51 -0600

    quick fix

--- tests/text/test_text_supervised.py
+++ tests/text/test_text_supervised.py
@@ -1,16 +1,3 @@
-from unittest.mock import patch
-from tests.common import MockProcess
-
-
-def mock_train(**kwargs):
-    str(kwargs)
-    return 1, 0
-
-
-def mock_text_preprocess(x_train):
-    return x_train
-
-
 def test_fit_predict():
     pass
 
