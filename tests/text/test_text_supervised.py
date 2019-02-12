29e7caea83e7788b0effc50343e3865f2423c6e1
Satya Kesav <satya@Satyas-MacBook-Pro.local>
2019-02-11 22:06:57 -0600

    minor fix

--- tests/text/test_text_supervised.py
+++ tests/text/test_text_supervised.py
@@ -11,15 +11,9 @@ def mock_text_preprocess(x_train):
     return x_train
 
 
-@patch('torch.multiprocessing.get_context', side_effect=MockProcess)
-@patch('autokeras.text.text_supervised.text_preprocess', side_effect=mock_text_preprocess)
-@patch('autokeras.search.ModelTrainer.train_model', side_effect=mock_train)
-def test_fit_predict(_, _1, _2):
+def test_fit_predict():
     pass
 
 
-@patch('torch.multiprocessing.get_context', side_effect=MockProcess)
-@patch('autokeras.search.ModelTrainer.train_model', side_effect=mock_train)
-@patch('autokeras.text.text_supervised.text_preprocess', side_effect=mock_text_preprocess)
-def test_evaluate(_, _1, _2):
+def test_evaluate():
     pass
