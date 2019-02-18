c011b0c6c5f7572bcc5c0522e4dbd438615ed21d
boyuangong <boyuangong@gmail.com>
2019-02-18 03:07:36 -0600

    remove - in sh file

--- torchaudio.sh
+++ torchaudio.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-- sudo apt-get install sox libsox-dev libsox-fmt-all
-- git clone https://github.com/pytorch/audio.git
-- cd audio && python setup.py install
+sudo apt-get install sox libsox-dev libsox-fmt-all
+git clone https://github.com/pytorch/audio.git
+cd audio && python setup.py install
