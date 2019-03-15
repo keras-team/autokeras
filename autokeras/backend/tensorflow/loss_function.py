1047541317c83650fa001e85fcb6a90af900253a
song3134 <song_3134@tamu.edu>
2019-03-15 17:36:21 -0500

    modify tf backend loss function

--- autokeras/backend/tensorflow/loss_function.py
+++ autokeras/backend/tensorflow/loss_function.py
@@ -2,12 +2,13 @@ import tensorflow as tf
 
 
 def classification_loss(prediction, label):
-    return tf.keras.losses.categorical_crossentropy(tf.nn.softmax(prediction), label)  # , from_logits=True
+    return tf.keras.losses.categorical_crossentropy(y_pred=tf.nn.softmax(prediction, axis=-1), y_true=label, axis=-1)  # ,from_logits=True
+    # return -tf.reduce_sum(tf.nn.log_softmax(prediction) * label, 1)
 
 
 def regression_loss(prediction, target):
-    return tf.keras.losses.mean_squared_error(prediction, target.float())
+    return tf.keras.losses.mean_squared_error(y_pred=prediction, y_true=target)
 
 
 def binary_classification_loss(prediction, label):
-    return tf.keras.losses.binary_crossentropy(tf.nn.sigmoid(prediction), label)  # , from_logits=True
+    return tf.keras.losses.binary_crossentropy(y_pred=tf.nn.sigmoid(prediction), y_true=label, axis=-1)  # ,from_logits=True
