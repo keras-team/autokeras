3df4f7b7910610a11dd2d95ff5186e94c717bf92
Yinxi Liu (FA Talent) <v-yinxli@microsoft.com>
2019-03-21 20:48:03 +0800

    use random number that is cryptographically secure in python

--- autokeras/pretrained/voice_generator/deepvoice3_pytorch/frontend.py
+++ autokeras/pretrained/voice_generator/deepvoice3_pytorch/frontend.py
@@ -1,36 +1,38 @@
 # coding: utf-8
+import os
+import nltk
 from autokeras.pretrained.voice_generator.deepvoice3_pytorch.text.symbols import symbols
 
-import nltk
-from random import random
 
-n_vocab = len(symbols)
+N_VOCAB = len(symbols)
+n_vocab = N_VOCAB
 
 try:
-    _arphabet = nltk.corpus.cmudict.dict()
-except:
+    _ARPHABET = nltk.corpus.cmudict.dict()
+except BaseException:
     nltk.download("cmudict")
-    _arphabet = nltk.corpus.cmudict.dict()
+    _ARPHABET = nltk.corpus.cmudict.dict()
 
 
-def _maybe_get_arpabet(word, p):
+def _maybe_get_arpabet(word, pro):
     try:
-        phonemes = _arphabet[word][0]
+        phonemes = _ARPHABET[word][0]
         phonemes = " ".join(phonemes)
     except KeyError:
         return word
 
-    return '{%s}' % phonemes if random() < p else word
+    return '{%s}' % phonemes if ord(os.urandom(1)) < pro else word
 
 
-def mix_pronunciation(text, p):
-    text = ' '.join(_maybe_get_arpabet(word, p) for word in text.split(' '))
+def mix_pronunciation(text, pro):
+    text = ' '.join(_maybe_get_arpabet(word, pro) for word in text.split(' '))
     return text
 
 
 def text_to_sequence(text, p=0.0):
-    if p >= 0:
-        text = mix_pronunciation(text, p)
+    pro = p
+    if pro >= 0:
+        text = mix_pronunciation(text, pro)
     from autokeras.pretrained.voice_generator.deepvoice3_pytorch.text.text import text_to_sequence
     text = text_to_sequence(text, ["english_cleaners"])
     return text
