a2edf27ded5e9c6fbd06e58dbceb7a5f4101bca0
KailinXu <44738198+KailinXu@users.noreply.github.com>
2019-03-30 14:45:42 +0800

    #issue_427 (#606)
    I have replacedthe word named assert, and slove the problem of duplication.
    Thanks for reading.

--- autokeras/pretrained/voice_generator/deepvoice3_pytorch/modules.py
+++ autokeras/pretrained/voice_generator/deepvoice3_pytorch/modules.py
@@ -38,7 +38,8 @@ class SinusoidalEncoding(nn.Embedding):
 
     def forward(self, x, w=1.0):
         isscaler = np.isscalar(w)
-        assert self.padding_idx is not None
+        if self.padding_idx is None:
+            raise AssertionError()
 
         if isscaler or w.size(0) == 1:
             weight = sinusoidal_encode(self.weight, w)
@@ -60,22 +61,23 @@ def embedding(num_embeddings, embedding_dim, padding_idx, std=0.01):
     m.weight.data.normal_(0, std)
     return m
 
+def m_modification(m, in_channels, dropout, std_mul):
+    std = math.sqrt((std_mul * (1.0 - dropout)) / (m.kernel_size[0] * in_channels))
+    m.weight.data.normal_(mean=0, std=std)
+    m.bias.data.zero_()
+    return m
 
 def conv1d(in_channels, out_channels, kernel_size, dropout=0, std_mul=4.0, **kwargs):
     from .conv import Conv1d
     m = Conv1d(in_channels, out_channels, kernel_size, **kwargs)
-    std = math.sqrt((std_mul * (1.0 - dropout)) / (m.kernel_size[0] * in_channels))
-    m.weight.data.normal_(mean=0, std=std)
-    m.bias.data.zero_()
+    m = m_modification(m,in_channels,dropout,std_mul)
     return nn.utils.weight_norm(m)
 
 
 def conv_transpose1d(in_channels, out_channels, kernel_size, dropout=0,
                      std_mul=1.0, **kwargs):
     m = nn.ConvTranspose1d(in_channels, out_channels, kernel_size, **kwargs)
-    std = math.sqrt((std_mul * (1.0 - dropout)) / (m.kernel_size[0] * in_channels))
-    m.weight.data.normal_(mean=0, std=std)
-    m.bias.data.zero_()
+    m = m_modification(m,in_channels,dropout,std_mul)
     return nn.utils.weight_norm(m)
 
 
