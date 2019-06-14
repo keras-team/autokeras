f0060c0e1b5b1043d1d5d2af08c3b829874ef0a9
Aqua Eustoma <yjbian92@gmail.com>
2019-06-14 10:38:30 -0700

    [MRG] (Tunable) XceptionBlock implemented (#653) (#666)
    * Jun 9, not yet
    
    * XceptionBlock
    
    * XceptionBlock
    
    * XceptionBlock
    
    * Tunable Xception Block
    
    * Tunable Xception Block
    
    * Tunable Xception Block

--- autokeras/hypermodel/hyper_block.py
+++ autokeras/hypermodel/hyper_block.py
@@ -165,85 +165,110 @@ class XceptionBlock(HyperBlock):
         output_node = input_node
         channel_axis = 1 if tf.keras.backend.image_data_format() == 'channels_first' else -1
 
-        # To decide the size of the architecture
-        filter_entry = hp.Choice('num_filters', [16, 32, 64], default=32)
-        filter_middle = int(filter_entry * 22.75)
-        filter_exit = int(filter_entry * 48)
+        #### Parameters ####
+        # [general]
+        kernel_size = hp.Range("kernel_size", 3, 5)
+        initial_strides = (2, 2)
+        activation = hp.Choice("activation", ["relu", "selu"])
+        # [Entry Flow]
+        conv2d_filters = hp.Choice("conv2d_num_filters", [32, 64, 128])
+        sep_filters = hp.Range("sep_num_filters", 128, 768)
+        # [Middle Flow]
+        residual_blocks = hp.Range("num_residual_blocks", 2, 8)
+        # [Exit Flow]
 
-        # Entry flow (original filters: 32, 64)
-        output_node = tf.keras.layers.Conv2D(filter_entry, (3, 3), strides=(2, 2), use_bias=False)(output_node)
-        output_node = tf.keras.layers.BatchNormalization(axis=channel_axis)(output_node)
-        output_node = tf.keras.layers.Activation('relu')(output_node)
-        output_node = tf.keras.layers.Conv2D(filter_entry*2, (3, 3), use_bias=False)(output_node)
-        output_node = tf.keras.layers.BatchNormalization(axis=channel_axis)(output_node)
-        output_node = tf.keras.layers.Activation('relu')(output_node)
-
-        # Entry flow (original filters: 128, 256, 728)
-        output_node = self._entry_flow(filter_entry*4, channel_axis, start_with_relu=False)(output_node)
-        output_node = self._entry_flow(filter_entry*8, channel_axis, start_with_relu=True)(output_node)
-        output_node = self._entry_flow(filter_middle, channel_axis, start_with_relu=True)(output_node)
-
-        # Middle flow (original filters: 728), repeated eight times
-        for i in range(8):
-            output_node = self._middle_flow(filter_middle, channel_axis)(output_node)
-
-        # Exit flow (original filters: 728, 1024; 1536, 2048)
-        output_node = self._exit_flow(filter_middle, filter_entry*32,
-                                      start_with_relu=True, with_shortcut=True)(output_node)
-        output_node = self._exit_flow(filter_exit, filter_entry*64,
-                                      start_with_relu=False, with_shortcut=False)(output_node)
+        #### Model ####
+        # Initial conv2d
+        dims = conv2d_filters
+        output_node = self._conv(dims, kernel_size=(kernel_size, kernel_size),
+                                 activation=activation, strides=initial_strides)(output_node)
+        # Separable convs
+        dims = sep_filters
+        for _ in range(residual_blocks):
+            output_node = self._residual(dims, activation=activation,
+                                         max_pooling=False, channel_axis=channel_axis)(output_node)
+        # Exit
+        dims *= 2
+        output_node = self._residual(dims, activation=activation,
+                                     max_pooling=True, channel_axis=channel_axis)(output_node)
 
         return output_node
 
-    def _entry_flow(self, filters, channel_axis=-1, start_with_relu=True):
+    @classmethod
+    def _sep_conv(cls, filters, kernel_size=(3, 3), activation='relu'):
         def func(x):
-            residual = tf.keras.layers.Conv2D(filters, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
-            residual = tf.keras.layers.BatchNormalization(axis=channel_axis)(residual)
-            if start_with_relu:
+            if activation == 'selu':
+                x = tf.keras.layers.SeparableConv2D(filters, kernel_size,
+                                                    activation='selu',
+                                                    padding='same',
+                                                    kernel_initializer='lecun_normal')(x)
+            elif activation == 'relu':
+                x = tf.keras.layers.SeparableConv2D(filters, kernel_size,
+                                                    padding='same',
+                                                    use_bias=False)(x)
+                x = tf.keras.layers.BatchNormalization()(x)
                 x = tf.keras.layers.Activation('relu')(x)
-            x = tf.keras.layers.SeparableConv2D(filters, (3, 3), padding='same', use_bias=False)(x)
-            x = tf.keras.layers.BatchNormalization(axis=channel_axis)(x)
-            x = tf.keras.layers.Activation('relu')(x)
-            x = tf.keras.layers.SeparableConv2D(filters, (3, 3), padding='same', use_bias=False)(x)
-            x = tf.keras.layers.BatchNormalization(axis=channel_axis)(x)
-            x = tf.keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(x)
-            return tf.keras.layers.add([x, residual])
+            else:
+                raise ValueError('Unknown activation function: {:s}'.format(activation))
+            return x
         return func
 
-    def _middle_flow(self, filters, channel_axis=-1):
+    @classmethod
+    def _residual(cls,
+                  filters, kernel_size=(3, 3), activation='relu',
+                  pool_strides=(2, 2), max_pooling=True,
+                  channel_axis=-1):
+        """ Residual block. """
         def func(x):
-            residual = x
-            x = tf.keras.layers.Activation('relu')(x)
-            x = tf.keras.layers.SeparableConv2D(filters, (3, 3), padding='same', use_bias=False)(x)
-            x = tf.keras.layers.BatchNormalization(axis=channel_axis)(x)
-            x = tf.keras.layers.Activation('relu')(x)
-            x = tf.keras.layers.SeparableConv2D(filters, (3, 3), padding='same', use_bias=False)(x)
-            x = tf.keras.layers.BatchNormalization(axis=channel_axis)(x)
-            x = tf.keras.layers.Activation('relu')(x)
-            x = tf.keras.layers.SeparableConv2D(filters, (3, 3), padding='same', use_bias=False)(x)
-            x = tf.keras.layers.BatchNormalization(axis=channel_axis)(x)
-            return tf.keras.layers.add([x, residual])
+            if max_pooling:
+                res = tf.keras.layers.Conv2D(filters, kernel_size=(1, 1), strides=pool_strides, padding='same')(x)
+            elif filters != tf.keras.backend.int_shape(x)[channel_axis]:
+                res = tf.keras.layers.Conv2D(filters, kernel_size=(1, 1), padding='same')(x)
+            else:
+                res = x
+
+            x = cls._sep_conv(filters, kernel_size, activation)(x)
+            x = cls._sep_conv(filters, kernel_size, activation)(x)
+            if max_pooling:
+                x = tf.keras.layers.MaxPool2D(kernel_size, strides=pool_strides, padding='same')(x)
+
+            x = tf.keras.layers.add([x, res])
+            return x
         return func
 
-    def _exit_flow(self, filter1, filter2, channel_axis=-1, start_with_relu=False, with_shortcut=False):
+    @classmethod
+    def _conv(cls, filters, kernel_size=(3, 3), activation='relu', strides=(2, 2)):
+        """ 2d convolution block. """
         def func(x):
-            if with_shortcut:
-                residual = tf.keras.layers.Conv2D(filter2, (1, 1), strides=(2, 2), padding='same', use_bias=False)(x)
-                residual = tf.keras.layers.BatchNormalization(axis=channel_axis)(residual)
-
-            if start_with_relu:
+            if activation == 'selu':
+                x = tf.keras.layers.Conv2D(filters, kernel_size, strides=strides, activation='selu',
+                                       padding='same', kernel_initializer='lecun_normal', bias_initializer='zeros')(x)
+            elif activation == 'relu':
+                x = tf.keras.layers.Conv2D(filters, kernel_size, strides=strides, padding='same', use_bias=False)(x)
+                x = tf.keras.layers.BatchNormalization()(x)
                 x = tf.keras.layers.Activation('relu')(x)
-            x = tf.keras.layers.SeparableConv2D(filter1, (3, 3), padding='same', use_bias=False)(x)
-            x = tf.keras.layers.BatchNormalization(axis=channel_axis)(x)
-            x = tf.keras.layers.Activation('relu')(x)
-            x = tf.keras.layers.SeparableConv2D(filter2, (3, 3), padding='same', use_bias=False)(x)
-            x = tf.keras.layers.BatchNormalization(axis=channel_axis)(x)
-            if not start_with_relu:
-                x = tf.keras.layers.Activation('relu')(x)
+            else:
+                raise ValueError('Unknown activation function: {:s}'.format(activation))
+            return x
+        return func
 
-            if with_shortcut:
-                x = tf.keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same')(x)
-                x = tf.keras.layers.add([x, residual])
+    @classmethod
+    def _dense(cls, dims, activation='relu', batchnorm=True, dropout_rate=0):
+        def func(x):
+            if activation == 'selu':
+                x = tf.keras.layers.Dense(dims, activation='selu',
+                                          kernel_initializer='lecun_normal',
+                                          bias_initializer='zeros')(x)
+                if dropout_rate:
+                    x = tf.keras.layers.AlphaDropout(dropout_rate)(x)
+            elif activation == 'relu':
+                x = tf.keras.layers.Dense(dims, activation='relu')(x)
+                if batchnorm:
+                    x = tf.keras.layers.BatchNormalization()(x)
+                if dropout_rate:
+                    x = tf.keras.layers.Dropout(dropout_rate)(x)
+            else:
+                raise ValueError('Unknown activation function: {:s}'.format(activation))
             return x
         return func
 
