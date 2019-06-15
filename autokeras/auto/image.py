43ef191c001ab51d996c451bc3b42ad2601271b4
Davidsirui <33369174+Davidsirui@users.noreply.github.com>
2019-06-15 21:00:43 +0800

    Use tf.data and tf.image for image augmentation
    A simple and scratchy try.

--- autokeras/auto/image.py
+++ autokeras/auto/image.py
@@ -7,6 +7,116 @@ from autokeras.hypermodel import hyper_block, hyper_node
 from autokeras.hypermodel import hyper_head
 from autokeras import tuner
 
+def augmentedDataGenerator(dataset='cifar10'):
+    # You give the dataset name, we return the augmented data!
+    if dataset == 'cifar10':
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+    elif dataset == 'cifar100':
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar100.load_data()
+    elif dataset == 'mnist':
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    else:
+        # Fasion-MNIST
+        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+    # Augment the images
+    for i in range(x_train.shape[0]):
+        x_train[i] = augment_image(x_train[i])
+    for i in range(x_test.shape[0]):
+        x_test[i] = augment_image(x_test[i])
+    return (x_train, y_train), (x_test, y_test)
+
+def _get_min_and_max(value, name):
+    if isinstance(value, (tuple, list)):
+        if len(value) != 2:
+            raise ValueError(
+                'Argument %s expected either a float between 0 and 1, '
+                'or a tuple of 2 floats between 0 and 1, but got: %s' % (value, name))
+            min_value = value[0]
+            max_value = value[1]
+        else:
+            min_value = 1. - value
+            max_value = 1. + value
+    return min_value, max_value
+
+
+def augment_image(image,
+                  rotation_range=0,  # either 0, 90, 180
+                  horizontal_crop_range=0.,  # fraction 0-1
+                  vertical_crop_range=0.,  # fraction 0-1
+                  brightness_range=0.,  # fraction 0-1  [X]
+                  saturation_range=0.,  # fraction 0-1  [X]
+                  contrast_range=0.,  # fraction 0-1  [X]
+                  horizontal_flip=False,  # boolean  [X]
+                  vertical_flip=False,
+                  scale_factor=0. , # scale factor
+                  translation_top=0. ,
+                  translation_bottome=0.,
+                  translation_left=0.,
+                  translation_right=0.,
+                  Gaussian_noise=False):  # boolean  [X]
+    target_height = image.shape[0]
+    target_width = image.shape[1]
+    if Gaussian_noise:
+        # TODO: Add Gaussian noise to the image
+        pass
+    if scale_factor:
+        # TODO: Scale the image using scikit-image
+        pass
+    if translation_bottome or translation_left or translation_right or translation_top:
+        # TODO; Translate the images
+        pass
+    if rotation_range:
+        if rotation_range == 90:
+            k_choices = {0, 1, 3}
+        elif rotation_range == 180:
+            k_choices = {0, 1, 2, 3}
+        # TODO
+    if brightness_range:
+        min_value, max_value = _get_min_and_max(
+            brightness_range, 'brightness_range')
+        image = tf.image.random_brightness(image, min_value, max_value)
+ 
+    if saturation_range:
+        min_value, max_value = _get_min_and_max(
+            brightness_range, 'saturation_range')
+        image = tf.image.random_saturation(x, min_value, max_value)
+
+    if contrast_range:
+        min_value, max_value = _get_min_and_max(
+            contrast_range, 'contrast_range')
+        image = tf.image.random_contrast(x, min_value, max_value)
+
+    if horizontal_crop_range or vertical_crop_range:
+        if image.shape.rank == 3:
+            height = image.shape[1]
+            width = image.shape[2]
+        else:
+            height = image.shape[0]
+            width = image.shape[1]
+
+        height_repartition_factor = random.random()
+        crop_height = int(height * vertical_crop_range)
+        offset_height = math.floor(height_repartition_factor * target_height)
+        target_height = offset_height + crop_height
+
+        width_repartition_factor = random.random()
+        crop_width = int(width * horizontal_crop_range)
+        offset_width = math.floor(width_repartition_factor * target_width)
+        target_width = offset_width + crop_width
+
+        image = tf.image.crop_to_bounding_box(
+            image,
+            offset_height,
+            offset_width,
+            target_height,
+            target_width)
+
+    if horizontal_flip:
+        image = tf.image.random_flip_left_right(image)
+    if vertical_flip:
+        image = tf.image.random_flip_up_down(image)
+    return image
+
 
 class ImageTuner(tuner.SequentialRandomSearch):
 
@@ -15,52 +125,10 @@ class ImageTuner(tuner.SequentialRandomSearch):
         model = self.hypermodel.build(hyperparameters)
 
         self._check_space(hyperparameters)
-
-        datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-            # rescale image pixels to [0, 1]
-            rescale=None,  # 1. / self.max_val,
-            # set input mean to 0 over the dataset
-            featurewise_center=False,
-            # set each sample mean to 0
-            samplewise_center=False,
-            # divide inputs by std of dataset
-            featurewise_std_normalization=False,
-            # divide each input by its std
-            samplewise_std_normalization=False,
-            # apply ZCA whitening
-            zca_whitening=False,
-            # randomly rotate images in the range (deg 0 to 180)
-            rotation_range=0,
-            # randomly shift images horizontally
-            width_shift_range=0.1,
-            # randomly shift images vertically
-            height_shift_range=0.1,
-            # randomly flip images
-            horizontal_flip=True,
-            # randomly flip images
-            vertical_flip=False)
-
-        datagen.fit(fit_kwargs['x'])
-        fit_kwargs['batch_size'] = min(len(fit_kwargs['x']),
-                                       fit_kwargs.get(
-                                           'batch_size',
-                                           const.Constant.BATCH_SIZE))
-        data_flow = datagen.flow(fit_kwargs['x'],
-                                 fit_kwargs['y'],
-                                 fit_kwargs['batch_size'],
-                                 shuffle=True)
-        temp_fit_kwargs = fit_kwargs.copy()
-        temp_fit_kwargs.pop('x', None)
-        temp_fit_kwargs.pop('y', None)
-        temp_fit_kwargs.pop('batch_size', None)
-
-        # Train model
-        history = model.fit_generator(data_flow, **temp_fit_kwargs)
-
-        metric_name = model.metrics_names[1]
-        feedback = history.history['val_' + metric_name][-1]
-        return model, feedback
-
+        
+        (x_train, y_train), (x_test, y_test) = AugmentedDataGenerator('cifar100')
+        loss = model.train(x_train,y_train)
+        return model, loss
 
 class SupervisedImagePipeline(auto_pipeline.AutoPipeline):
 
