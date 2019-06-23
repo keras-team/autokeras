e9eacbf27ac75ed895eccc8ae717834e03314f29
Praveen Venugopal <razorvine@tamu.edu>
2019-06-23 16:51:24 -0500

    improved rnnblock commit 1

--- autokeras/hypermodel/hyper_head.py
+++ autokeras/hypermodel/hyper_head.py
@@ -55,3 +55,24 @@ class RegressionHead(HyperHead):
             output_node = tf.keras.layers.Dense(self.output_shape[-1])(output_node)
             return output_node
         return hyper_block.Reshape(self.output_shape).build(hp, output_node)
+
+
+class SequenceHead(HyperHead):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if not self.metrics:
+            self.metrics = [tf.keras.metrics.mse]
+        if not self.loss:
+            self.loss = tf.keras.losses.mean_squared_error
+
+    def build(self, hp, inputs=None):
+        input_node = layer_utils.format_inputs(inputs, self.name, num=1)[0]
+        output_node = input_node
+        if len(self.output_shape) == 1:
+            raise AttributeError(
+                "SequenceHead expects input of atleast 2 dimensions [time_steps,feature_size] but got {shape}".format(
+                    shape=input_node.shape))
+            # output_node = hyper_block.Flatten().build(hp, output_node)
+        # TODO: Forced conversion exists in Dense layer. Must generalize for any sequence shape in enc and dec
+        output_node = tf.keras.layers.Dense(self.output_shape[-1],activation='softmax')(output_node)
+        return output_node
