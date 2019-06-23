abe6532fdad57172b79a5483e570bfe5cee99284
Davidsirui <33369174+Davidsirui@users.noreply.github.com>
2019-06-23 12:14:13 +0800

    Update image.py

--- autokeras/auto/image.py
+++ autokeras/auto/image.py
@@ -7,131 +7,102 @@ from autokeras.hypermodel import hyper_block, hyper_node
 from autokeras.hypermodel import hyper_head
 from autokeras import tuner
 
-def augmentedDataGenerator(dataset='cifar10'):
-    # You give the dataset name, we return the augmented data!
-    if dataset == 'cifar10':
-        (x_train, y_train),(x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-    elif dataset == 'cifar100':
-        (x_train, y_train),(x_test, y_test) = tf.keras.datasets.cifar100.load_data()
-    elif dataset == 'mnist':
-        (x_train, y_train),(x_test, y_test) = tf.keras.datasets.mnist.load_data()
-    else:
-        # Fasion-MNIST
-        (x_train, y_train),(x_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
-    # Augment the images
-    for i in range(x_train.shape[0]):
-        x_train[i] = augment_image(x_train[i])
-    y_train = tf.keras.utils.to_categorical(y_train,num_classes=None)
-    return (x_train, y_train)
-
-def _get_min_and_max(value, name):
-    if isinstance(value, (tuple, list)):
-        if len(value) != 2:
-            raise ValueError(
-                'Argument %s expected either a float between 0 and 1, '
-                'or a tuple of 2 floats between 0 and 1, but got: %s' % (value, name))
+def get_min_and_max(value, name):
+        if isinstance(value, (tuple, list)):
+            if len(value) != 2:
+                raise ValueError(
+                    'Argument %s expected either a float between 0 and 1, '
+                    'or a tuple of 2 floats between 0 and 1, but got: %s' % (value, name))
             min_value = value[0]
             max_value = value[1]
         else:
             min_value = 1. - value
             max_value = 1. + value
-    return min_value, max_value
+        return min_value, max_value
 
+def augment_image(    x_train,
+                      rotation_range=0,  # either 0, 90, 180, 270
+                      random_crop_height=0,  # fraction 0-1
+                      random_crop_width=0,  # fraction 0-1
+                      random_crop_seed=0,   # positice number
+                      brightness_range=0.,  # fraction 0-1  [X]
+                      saturation_range=0.,  # fraction 0-1  [X]
+                      contrast_range=0.,  # fraction 0-1  [X]
+                      horizontal_flip=False,  # boolean  [X]
+                      vertical_flip=False,
+                      translation_top=0,  # translation_top are the number of pixels, so positive int type are required.
+                      translation_bottom=0,
+                      translation_left=0,
+                      translation_right=0,
+                      gaussian_noise=False):  # boolean  [X]
+    x_train = tf.convert_to_tensor(x_train)
+    length_dim = len(x_train.shape)
+    if length_dim != 4:
+        raise ValueError(
+            'The input of x_train should be a [batch_size, height, width, channel] shape tensor or list, but we get %s' % (x_train.shape))
+    batch_num = x_train.shape[0]
+    target_height = x_train.shape[1]
+    target_width = x_train.shape[2]
+    channels = x_train.shape[3]
+    dataset = tf.data.Dataset.from_tensor_slices(x_train)
+    dataset = dataset.batch(batch_size=batch_num)
+    iterator = dataset.make_one_shot_iterator()
+    one_element = iterator.get_next()
+    with tf.Session() as sess:
+        for i in range(1):
+            batch = sess.run([one_element])
+            image = tf.convert_to_tensor(batch[0])
+            image = tf.cast(image,dtype=tf.float32)
+            if gaussian_noise:
+                noise = tf.random_normal(shape=tf.shape(image), mean=0.0, stddev=1.0, dtype=tf.float32)
+                image = tf.add(image, noise)
 
-def augment_image(image,
-                  rotation_range=0,  # either 0, 90, 180
-                  horizontal_crop_range=0.,  # fraction 0-1
-                  vertical_crop_range=0.,  # fraction 0-1
-                  brightness_range=0.,  # fraction 0-1  [X]
-                  saturation_range=0.,  # fraction 0-1  [X]
-                  contrast_range=0.,  # fraction 0-1  [X]
-                  horizontal_flip=False,  # boolean  [X]
-                  vertical_flip=False,
-                  scale_factor=0. , # scale factor
-                  translation_top=0. ,
-                  translation_bottome=0.,
-                  translation_left=0.,
-                  translation_right=0.,
-                  Gaussian_noise=False):  # boolean  [X]
-    target_height = image.shape[0]
-    target_width = image.shape[1]
-    if Gaussian_noise:
-        # TODO: Add Gaussian noise to the image
-        pass
-    if scale_factor:
-        # TODO: Scale the image using scikit-image
-        pass
-    if translation_bottome or translation_left or translation_right or translation_top:
-        # TODO; Translate the images
-        pass
-    if rotation_range:
-        if rotation_range == 90:
-            k_choices = {0, 1, 3}
-        elif rotation_range == 180:
-            k_choices = {0, 1, 2, 3}
-        # TODO
-    if brightness_range:
-        min_value, max_value = _get_min_and_max(
-            brightness_range, 'brightness_range')
-        image = tf.image.random_brightness(image, min_value, max_value)
- 
-    if saturation_range:
-        min_value, max_value = _get_min_and_max(
-            brightness_range, 'saturation_range')
-        image = tf.image.random_saturation(x, min_value, max_value)
+            if translation_bottom or translation_left or translation_right or translation_top:
+                x = tf.image.pad_to_bounding_box(image, translation_top, translation_left,
+                                                 target_height + translation_bottom + translation_top,
+                                                 target_width + translation_right + translation_left)
+                image = tf.image.crop_to_bounding_box(x, translation_bottom, translation_right, target_height,
+                                                      target_width)
 
-    if contrast_range:
-        min_value, max_value = _get_min_and_max(
-            contrast_range, 'contrast_range')
-        image = tf.image.random_contrast(x, min_value, max_value)
+            if rotation_range:
+                if rotation_range == 90:
+                    image = tf.image.rot90(image, k=1)
+                elif rotation_range == 180:
+                    image = tf.image.rot90(image, k=2)
+                elif rotation_range == 270:
+                    image = tf.image.rot90(image, k=3)
+                else:
+                    image = tf.image.rot90(image, k=4)
 
-    if horizontal_crop_range or vertical_crop_range:
-        if image.shape.rank == 3:
-            height = image.shape[1]
-            width = image.shape[2]
-        else:
-            height = image.shape[0]
-            width = image.shape[1]
+            if brightness_range:
+                min_value, max_value = get_min_and_max(
+                    brightness_range, 'brightness_range')
+                image = tf.image.random_brightness(image, min_value, max_value)
 
-        height_repartition_factor = random.random()
-        crop_height = int(height * vertical_crop_range)
-        offset_height = math.floor(height_repartition_factor * target_height)
-        target_height = offset_height + crop_height
+            if saturation_range:
+                min_value, max_value = get_min_and_max(
+                    saturation_range, 'saturation_range')
+                print(min_value,max_value)
+                image = tf.image.random_saturation(image, min_value, max_value)
 
-        width_repartition_factor = random.random()
-        crop_width = int(width * horizontal_crop_range)
-        offset_width = math.floor(width_repartition_factor * target_width)
-        target_width = offset_width + crop_width
+            if contrast_range:
+                min_value, max_value = get_min_and_max(
+                    contrast_range, 'contrast_range')
+                image = tf.image.random_contrast(image, min_value, max_value)
 
-        image = tf.image.crop_to_bounding_box(
-            image,
-            offset_height,
-            offset_width,
-            target_height,
-            target_width)
+            if random_crop_height and random_crop_width:
+                crop_size = [batch_num, random_crop_height, random_crop_width, channels]
+                seed = np.random.randint(random_crop_seed)
+                target_shape = (target_height,target_width)
+                print(tf.random_crop(image, size=crop_size, seed=seed).shape)
+                image = tf.image.resize_images(tf.random_crop(image, size=crop_size, seed=seed),size=target_shape)
 
-    if horizontal_flip:
-        image = tf.image.random_flip_left_right(image)
-    if vertical_flip:
-        image = tf.image.random_flip_up_down(image)
+            if horizontal_flip:
+                image = tf.image.flip_left_right(image)
+
+            if vertical_flip:
+                image = tf.image.flip_up_down(image)
     return image
-
-
-class ImageTuner(tuner.SequentialRandomSearch):
-
-    def _run(self, hyperparameters, fit_kwargs):
-        # Build a model instance.
-        model = self.hypermodel.build(hyperparameters)
-
-        self._check_space(hyperparameters)
-        
-        (x_train, y_train) = augmentedDataGenerator('cifar100')
-        
-        history = model.fit(x_train,y_train,batch_size=32)
-        metric_name = model.metrics_names[1]
-        feedback = history.history['val_' + metric_name][-1]
-        
-        return model, feedback
 
 class SupervisedImagePipeline(auto_pipeline.AutoPipeline):
 
