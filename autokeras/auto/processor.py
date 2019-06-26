a0c9f86933cf0a7e80116ae9a14443239992ab7d
Davidsirui <33369174+Davidsirui@users.noreply.github.com>
2019-06-26 14:51:39 +0800

    Data Augment in class Normalizer

--- autokeras/auto/processor.py
+++ autokeras/auto/processor.py
@@ -76,3 +76,101 @@ class Normalizer(object):
         # channel-wise normalize the image
         data = (data - self.mean) / self.std
         return data
+
+    def get_min_and_max(value, name):
+        if isinstance(value, (tuple, list)):
+            if len(value) != 2:
+                raise ValueError(
+                    'Argument %s expected either a float between 0 and 1, '
+                    'or a tuple of 2 floats between 0 and 1, but got: %s' % (value, name))
+            min_value = value[0]
+            max_value = value[1]
+        else:
+            min_value = 1. - value
+            max_value = 1. + value
+        return min_value, max_value
+    
+    def augment_image(self,    
+                      x_train,
+                      rotation_range=0,  # either 0, 90, 180, 270
+                      random_crop_height=0,  # fraction 0-1
+                      random_crop_width=0,  # fraction 0-1
+                      random_crop_seed=0,   # positice number
+                      brightness_range=0.,  # fraction 0-1  [X]
+                      saturation_range=0.,  # fraction 0-1  [X]
+                      contrast_range=0.,  # fraction 0-1  [X]
+                      horizontal_flip=False,  # boolean  [X]
+                      vertical_flip=False,
+                      translation_top=0,  # translation_top are the number of pixels, so positive int type are required.
+                      translation_bottom=0,
+                      translation_left=0,
+                      translation_right=0,
+                      gaussian_noise=False):  # boolean  [X]
+    x_train = tf.convert_to_tensor(x_train)
+    length_dim = len(x_train.shape)
+    if length_dim != 4:
+        raise ValueError(
+            'The input of x_train should be a [batch_size, height, width, channel] shape tensor or list, but we get %s' % (x_train.shape))
+    batch_num = x_train.shape[0]
+    target_height = x_train.shape[1]
+    target_width = x_train.shape[2]
+    channels = x_train.shape[3]
+    dataset = tf.data.Dataset.from_tensor_slices(x_train)
+    dataset = dataset.batch(batch_size=batch_num)
+    iterator = dataset.make_one_shot_iterator()
+    one_element = iterator.get_next()
+    with tf.Session() as sess:
+        for i in range(1):
+            batch = sess.run([one_element])
+            image = tf.convert_to_tensor(batch[0])
+            image = tf.cast(image,dtype=tf.float32)
+            if gaussian_noise:
+                noise = tf.random_normal(shape=tf.shape(image), mean=0.0, stddev=1.0, dtype=tf.float32)
+                image = tf.add(image, noise)
+
+            if translation_bottom or translation_left or translation_right or translation_top:
+                x = tf.image.pad_to_bounding_box(image, translation_top, translation_left,
+                                                 target_height + translation_bottom + translation_top,
+                                                 target_width + translation_right + translation_left)
+                image = tf.image.crop_to_bounding_box(x, translation_bottom, translation_right, target_height,
+                                                      target_width)
+
+            if rotation_range:
+                if rotation_range == 90:
+                    image = tf.image.rot90(image, k=1)
+                elif rotation_range == 180:
+                    image = tf.image.rot90(image, k=2)
+                elif rotation_range == 270:
+                    image = tf.image.rot90(image, k=3)
+                else:
+                    image = tf.image.rot90(image, k=4)
+
+            if brightness_range:
+                min_value, max_value = get_min_and_max(
+                    brightness_range, 'brightness_range')
+                image = tf.image.random_brightness(image, min_value, max_value)
+
+            if saturation_range:
+                min_value, max_value = get_min_and_max(
+                    saturation_range, 'saturation_range')
+                print(min_value,max_value)
+                image = tf.image.random_saturation(image, min_value, max_value)
+
+            if contrast_range:
+                min_value, max_value = get_min_and_max(
+                    contrast_range, 'contrast_range')
+                image = tf.image.random_contrast(image, min_value, max_value)
+
+            if random_crop_height and random_crop_width:
+                crop_size = [batch_num, random_crop_height, random_crop_width, channels]
+                seed = np.random.randint(random_crop_seed)
+                target_shape = (target_height,target_width)
+                print(tf.random_crop(image, size=crop_size, seed=seed).shape)
+                image = tf.image.resize_images(tf.random_crop(image, size=crop_size, seed=seed),size=target_shape)
+
+            if horizontal_flip:
+                image = tf.image.flip_left_right(image)
+
+            if vertical_flip:
+                image = tf.image.flip_up_down(image)
+    return image
