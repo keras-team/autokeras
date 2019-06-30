60877fb4dce79450288476d2b73835190312b509
Haifeng Jin <jhfjhfj1@gmail.com>
2019-06-30 12:09:58 -0700

    docstring added to auto tuner.

--- autokeras/auto/tuner.py
+++ autokeras/auto/tuner.py
@@ -3,8 +3,10 @@ import kerastuner
 
 
 class AutoTuner(kerastuner.Tuner):
+    """Modified KerasTuner base class to include preprocessing layers."""
 
     def run_trial(self, trial, hp, fit_args, fit_kwargs):
+        """Preprocess the x and y before calling the base run_trial."""
         fit_kwargs.update(dict(zip(tf.keras.Model.fit.__code__.co_varnames,
                                    fit_args)))
         fit_args = []
@@ -15,10 +17,26 @@ class AutoTuner(kerastuner.Tuner):
         super(AutoTuner, self).run_trial(trial, hp, fit_args, fit_kwargs)
 
     def get_best_hp(self, num_models=1):
+        """Returns hyperparameters used to build the best model(s).
+
+        Args:
+            num_models (int, optional): Number of best models, whose building
+                HyperParameters to return. Models will be returned in sorted order
+                starting from the best. Defaults to 1.
+
+        Returns:
+            List of HyperParameter instances.
+        """
         best_trials = self._get_best_trials(num_models)
         return [trial.hyperparameters.copy()
                 for trial in best_trials]
 
 
 class RandomSearch(AutoTuner, kerastuner.RandomSearch):
+    """KerasTuner RandomSearch with preprocessing layer tuning."""
+    pass
+
+
+class HyperBand(AutoTuner, kerastuner.Hyperband):
+    """KerasTuner Hyperband with preprocessing layer tuning."""
     pass
