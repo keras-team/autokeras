08b5db3112ab6c1e661e529477aebd6a3ea62e8e
Haifeng Jin <jhfjhfj1@gmail.com>
2019-07-13 16:56:19 -0700

    test changes

--- tests/test_hyper_block.py
+++ tests/test_hyper_block.py
@@ -63,28 +63,7 @@ def test_conv_block(tmp_dir):
     assert result.shape == (100, 10)
 
 
-def test_conv_block(tmp_dir):
-    x_train = np.random.rand(100, 3, 3, 3)
-    y_train = np.random.randint(10, size=100)
-    y_train = tf.keras.utils.to_categorical(y_train)
-
-    input_node = ak.Input()
-    output_node = input_node
-    output_node = ak.ConvBlock()(output_node)
-    output_node = ak.ClassificationHead()(output_node)
-
-    input_node.shape = (3, 3, 3)
-    output_node[0].shape = (10,)
-
-    graph = ak.GraphAutoModel(input_node, output_node, directory=tmp_dir)
-    model = graph.build(kerastuner.HyperParameters())
-    model.fit(x_train, y_train, epochs=1, batch_size=100, verbose=False)
-    result = model.predict(x_train)
-
-    assert result.shape == (100, 10)
-
-
-def test_rnn_block():
+def test_rnn_block(tmp_dir):
     x_train = np.random.rand(100, 32, 10, 10)
     y_train = np.random.randint(5, size=100)
     y_train = tf.keras.utils.to_categorical(y_train)
@@ -97,10 +76,15 @@ def test_rnn_block():
     input_node.shape = (32, 10, 10)
     output_node[0].shape = (5,)
 
-    graph = ak.GraphAutoModel(input_node, output_node)
-    model = graph.build(kerastuner.HyperParameters())
-    model.fit(x_train, y_train, epochs=1, batch_size=100, verbose=False)
-    result = model.predict(x_train)
+    graph = ak.GraphAutoModel(input_node, output_node,
+                              directory=tmp_dir,
+                              max_trials=1)
+    graph.fit(x_train, y_train,
+              epochs=1,
+              batch_size=100,
+              verbose=False,
+              validation_split=0.2)
+    result = graph.predict(x_train)
 
     assert result.shape == (100, 5)
 
