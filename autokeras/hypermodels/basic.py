7722a12b76fbf418c54615b6e5b06de7d6530a6c
Praveen Venugopal <razorvine@tamu.edu>
2019-07-31 14:26:11 -0500

    attention at cell level

--- autokeras/hypermodels/basic.py
+++ autokeras/hypermodels/basic.py
@@ -85,19 +85,33 @@ class RNNBlock(block_module.Block):
             be tuned automatically.
         layer_type: String. 'gru' or 'lstm'. If left unspecified, it will be tuned
             automatically.
+        attention: Boolean. Apply Recurrent attention. If left unspecified, it will
+            be tuned automatically. Does not go together with Bidirectional
     """
 
     def __init__(self,
-                 return_sequences=False,
+                 return_sequences=True,
                  bidirectional=None,
                  num_layers=None,
                  layer_type=None,
+                 attention=None,
                  **kwargs):
         super().__init__(**kwargs)
         self.return_sequences = return_sequences
         self.bidirectional = bidirectional
         self.num_layers = num_layers
         self.layer_type = layer_type
+        self.attention = attention
+
+    @staticmethod
+    def attention_over_time(inputs):
+        time_steps = int(inputs.shape[1])
+        context_vector = tf.keras.layers.Permute((2, 1))(inputs)
+        context_vector = tf.keras.layers.Dense(time_steps,
+                                              activation='softmax')(context_vector)
+        context_vector = tf.keras.layers.Permute((2, 1))(context_vector)
+        mul_attention_out = tf.keras.layers.Multiply()([inputs, context_vector])
+        return mul_attention_out
 
     def get_config(self):
         config = super().get_config()
@@ -131,11 +145,15 @@ class RNNBlock(block_module.Block):
         num_layers = self.num_layers or hp.Choice('num_layers',
                                                   [1, 2, 3],
                                                   default=2)
+        attention = self.attention or hp.Choice('attention',
+                                                [True, False],
+                                                default=True)
         rnn_layers = {
             'gru': layers.GRU,
             'lstm': layers.LSTM
         }
         in_layer = rnn_layers[layer_type]
+
         for i in range(num_layers):
             return_sequences = True
             if i == num_layers - 1:
@@ -148,6 +166,8 @@ class RNNBlock(block_module.Block):
                 output_node = in_layer(
                     feature_size,
                     return_sequences=return_sequences)(output_node)
+                output_node = self.attention_over_time(
+                    output_node) if attention else output_node
         return output_node
 
 
