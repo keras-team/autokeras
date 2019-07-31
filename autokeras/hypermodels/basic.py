23f080921a3198efc3ef34a0d4789f86b235b6cc
Praveen Venugopal <razorvine@tamu.edu>
2019-07-31 14:42:04 -0500

    pep8 fail fix

--- autokeras/hypermodels/basic.py
+++ autokeras/hypermodels/basic.py
@@ -107,8 +107,8 @@ class RNNBlock(block_module.Block):
     def attention_over_time(inputs):
         time_steps = int(inputs.shape[1])
         context_vector = tf.keras.layers.Permute((2, 1))(inputs)
-        context_vector = tf.keras.layers.Dense(time_steps,
-                                              activation='softmax')(context_vector)
+        context_vector = tf.keras.layers.Dense(time_steps, activation='softmax')(
+            context_vector)
         context_vector = tf.keras.layers.Permute((2, 1))(context_vector)
         mul_attention_out = tf.keras.layers.Multiply()([inputs, context_vector])
         return mul_attention_out
