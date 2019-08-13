3d0c9d131262f1dc1e1ba6f2746c7591d33a2db5
Haifeng Jin <jhfjhfj1@gmail.com>
2019-08-13 11:48:37 -0700

    fixing ci

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Data Analytics at Texas A&M (DATA) Lab, Keras Team',
     author_email='jhfjhfj1@gmail.com',
     url='http://autokeras.com',
-    download_url='https://github.com/keras-team/autokeras/archive/1.0.0.tar.gz',
+    download_url='https://github.com/keras-team/autokeras/archive/1.0.0a0.tar.gz',
     keywords=['AutoML', 'keras'],
     install_requires=[
         'tensorflow>=2.0.0b1',
