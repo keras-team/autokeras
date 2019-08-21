480b1bedc8a5b83abbd7cd0c68f7eb48b472ff22
Haifeng Jin <jhfjhfj1@gmail.com>
2019-08-21 14:46:56 -0500

    temp commit

---
+++ docs/templates/tutorial.md
@@ -0,0 +1,7 @@
+# AutoKeras 1.0 Tutorial
+
+In AutoKeras, there are 3 levels of APIs: task API, IO API, and functional API.
+
+## Task API
+## IO API
+## Functional API
---
+++ examples/task_api.py
@@ -0,0 +1,13 @@
+import autokeras as ak
+from keras.datasets import mnist
+
+if __name__ == '__main__':
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.reshape(x_train.shape + (1,))
+    x_test = x_test.reshape(x_test.shape + (1,))
+    clf = ak.ImageClassifier()
+    clf.fit(x_train, y_train, time_limit=30 * 60)
+    clf.final_fit(x_train, y_train, x_test, y_test, retrain=True)
+    y = clf.evaluate(x_test, y_test)
+
+    print(y * 100)
