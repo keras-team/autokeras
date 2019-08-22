589459be716686e042a84f35c0a9d6ebc8e39bb8
Haifeng Jin <jhfjhfj1@gmail.com>
2019-08-22 11:09:18 -0500

    cleaning styles of early stopping

--- tests/test_tuner.py
+++ tests/test_tuner.py
@@ -3,14 +3,15 @@ from autokeras.tuner import AutoTuner
 from tensorflow.keras.callbacks import EarlyStopping
 
 
-@pytest.fixture(scope='module')
 def test_add_earlystopping_callback():
     callbacks = []
     callbacks = AutoTuner.add_earlystopping_callback(callbacks)
     assert len(callbacks) == 1
-    assert callbacks[0].__class__.__name__ == 'EarlyStopping'
+    assert isinstance(callbacks[0], EarlyStopping)
 
+
+def test_not_add_earlystopping_callback():
     callbacks = [EarlyStopping(patience=2)]
     callbacks = AutoTuner.add_earlystopping_callback(callbacks)
     assert len(callbacks) == 1
-    assert callbacks[0].__class__.__name__ == 'EarlyStopping'
+    assert isinstance(callbacks[0], EarlyStopping)
