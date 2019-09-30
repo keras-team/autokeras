382977610bb3991b3f2f9fe4127504b8018a9f64
Haifeng Jin <jhfjhfj1@gmail.com>
2019-09-30 22:05:27 +0000

    improved automodel tests

--- tests/autokeras/auto_model_test.py
+++ tests/autokeras/auto_model_test.py
@@ -12,25 +12,8 @@ def tmp_dir(tmpdir_factory):
     return tmpdir_factory.mktemp('test_auto_model')
 
 
-def test_basics(tmp_dir):
-    x_train = np.random.rand(100, 32)
-    y_train = np.random.rand(100, 1)
-
-    input_node = ak.Input()
-    output_node = input_node
-    output_node = ak.DenseBlock()(output_node)
-    output_node = ak.RegressionHead()(output_node)
-
-    auto_model = ak.GraphAutoModel(input_node,
-                                   output_node,
-                                   directory=tmp_dir,
-                                   max_trials=1)
-    auto_model.fit(x_train, y_train, epochs=1, validation_data=(x_train, y_train))
-    result = auto_model.predict(x_train)
-    assert result.shape == (100, 1)
-
-
-def test_evaluate(tmp_dir):
+@mock.patch('autokeras.tuner.RandomSearch')
+def test_evaluate(tuner, tmp_dir):
     x_train = np.random.rand(100, 32)
     y_train = np.random.rand(100, 1)
 
@@ -45,67 +28,11 @@ def test_evaluate(tmp_dir):
                                    max_trials=1)
     auto_model.fit(x_train, y_train, epochs=1, validation_data=(x_train, y_train))
     auto_model.evaluate(x_train, y_train)
-
-
-def test_merge(tmp_dir):
-    x_train = np.random.rand(100, 33)
-    y_train = np.random.rand(100, 1)
-
-    input_node1 = ak.Input()
-    input_node2 = ak.Input()
-    output_node1 = ak.DenseBlock()(input_node1)
-    output_node2 = ak.DenseBlock()(input_node2)
-    output_node = ak.Merge()([output_node1, output_node2])
-    output_node = ak.RegressionHead()(output_node)
-
-    graph = ak.GraphAutoModel([input_node1, input_node2],
-                              output_node,
-                              directory=tmp_dir,
-                              max_trials=1)
-    graph.fit([x_train, x_train], y_train,
-              epochs=1,
-              batch_size=100,
-              verbose=False,
-              validation_split=0.5)
-    result = graph.predict([x_train, x_train])
-
-    assert result.shape == (100, 1)
-
-
-@mock.patch('kerastuner.engine.tuner.Tuner.search',
-            side_effect=lambda *args, **kwargs: None)
-def test_preprocessing(_, tmp_dir):
-    x_train = np.random.rand(100, 33)
-    y_train = np.random.rand(100, 1)
-
-    input_node1 = ak.Input()
-    temp_node1 = ak.Normalization()(input_node1)
-    output_node1 = ak.DenseBlock()(temp_node1)
-
-    output_node3 = ak.Normalization()(temp_node1)
-    output_node3 = ak.DenseBlock()(output_node3)
-
-    input_node2 = ak.Input()
-    output_node2 = ak.Normalization()(input_node2)
-    output_node2 = ak.DenseBlock()(output_node2)
-
-    output_node = ak.Merge()([output_node1, output_node2, output_node3])
-    output_node = ak.RegressionHead()(output_node)
-
-    graph = ak.GraphAutoModel([input_node1, input_node2],
-                              output_node,
-                              directory=tmp_dir,
-                              max_trials=1)
-    graph.fit([x_train, x_train], y_train,
-              epochs=1,
-              batch_size=100,
-              validation_data=([x_train, x_train], y_train),
-              validation_split=0.5,
-              verbose=False)
+    assert tuner.called
 
 
 @mock.patch('autokeras.tuner.RandomSearch')
-def test_auto_model_basic(_, tmp_dir):
+def test_auto_model_predict(tuner, tmp_dir):
     x_train = np.random.rand(100, 32, 32, 3)
     y_train = np.random.rand(100, 1)
 
@@ -114,27 +41,5 @@ def test_auto_model_basic(_, tmp_dir):
                               directory=tmp_dir,
                               max_trials=2)
     auto_model.fit(x_train, y_train, epochs=2, validation_split=0.2)
-
-
-def test_structured_data_input(tmp_dir):
-    num_data = 500
-    data = common.generate_structured_data(num_data)
-    x_train = data
-    y = np.random.randint(0, 3, num_data)
-    y_train = y
-
-    input_node = ak.StructuredDataInput(
-        column_names=common.COLUMN_NAMES_FROM_NUMPY,
-        column_types=common.COLUMN_TYPES_FROM_NUMPY)
-    output_node = input_node
-    output_node = ak.StructuredDataBlock()(output_node)
-    output_node = ak.ClassificationHead(loss='categorical_crossentropy',
-                                        metrics=['accuracy'])(output_node)
-
-    auto_model = ak.GraphAutoModel(input_node,
-                                   output_node,
-                                   directory=tmp_dir,
-                                   max_trials=1)
-    auto_model.fit(x_train, y_train, epochs=1,
-                   validation_data=(x_train, y_train))
     auto_model.predict(x_train)
+    assert tuner.called
--- tests/autokeras/graph_test.py
+++ tests/autokeras/graph_test.py
@@ -1,8 +1,13 @@
+from unittest import mock
+
 import kerastuner
 from kerastuner.engine import hyperparameters as hp_module
+import tensorflow as tf
 import pytest
 
 import autokeras as ak
+from autokeras.hypermodel import graph as graph_module
+from tests import common
 
 
 def test_set_hp():
@@ -13,7 +18,7 @@ def test_set_hp():
     head.output_shape = (1,)
     output_node = head(output_node)
 
-    graph = ak.hypermodel.graph.HyperBuiltGraphHyperModel(input_node, output_node)
+    graph = graph_module.HyperBuiltGraphHyperModel(input_node, output_node)
     hp = kerastuner.HyperParameters()
     graph.set_hps([hp_module.Choice('dense_block_1/num_layers', [6], default=6)])
     graph.build(hp)
@@ -37,7 +42,7 @@ def test_input_output_disconnect():
     output_node = ak.RegressionHead()(output_node)
 
     with pytest.raises(ValueError) as info:
-        ak.hypermodel.graph.GraphHyperModel(input_node1, output_node)
+        graph_module.GraphHyperModel(input_node1, output_node)
     assert 'Inputs and outputs not connected.' in str(info.value)
 
 
@@ -52,8 +57,7 @@ def test_hyper_graph_cycle():
     head.outputs = output_node1
 
     with pytest.raises(ValueError) as info:
-        ak.hypermodel.graph.GraphHyperModel([input_node1, input_node2],
-                                            output_node)
+        graph_module.GraphHyperModel([input_node1, input_node2], output_node)
     assert 'The network has a cycle.' in str(info.value)
 
 
@@ -66,5 +70,72 @@ def test_input_missing():
     output_node = ak.RegressionHead()(output_node)
 
     with pytest.raises(ValueError) as info:
-        ak.hypermodel.graph.GraphHyperModel(input_node1, output_node)
+        graph_module.GraphHyperModel(input_node1, output_node)
     assert 'A required input is missing for HyperModel' in str(info.value)
+
+
+def test_graph_basics():
+    input_node = ak.Input(shape=(30,))
+    output_node = input_node
+    output_node = ak.DenseBlock()(output_node)
+    output_node = ak.RegressionHead(output_shape=(1,))(output_node)
+
+    graph = graph_module.HyperBuiltGraphHyperModel(input_node, output_node)
+    model = graph.build(kerastuner.HyperParameters())
+    assert model.input_shape == (None, 30)
+    assert model.output_shape == (None, 1)
+
+
+def test_merge():
+    input_node1 = ak.Input(shape=(30,))
+    input_node2 = ak.Input(shape=(40,))
+    output_node1 = ak.DenseBlock()(input_node1)
+    output_node2 = ak.DenseBlock()(input_node2)
+    output_node = ak.Merge()([output_node1, output_node2])
+    output_node = ak.RegressionHead(output_shape=(1,))(output_node)
+
+    graph = graph_module.HyperBuiltGraphHyperModel([input_node1, input_node2],
+                                                   output_node)
+    model = graph.build(kerastuner.HyperParameters())
+    assert model.input_shape == [(None, 30), (None, 40)]
+    assert model.output_shape == (None, 1)
+
+
+def test_preprocessing():
+    input_shape = (33,)
+    output_shape = (1,)
+    x_train_1 = common.generate_data(
+        num_instances=100,
+        shape=input_shape,
+        dtype='dataset')
+    x_train_2 = common.generate_data(
+        num_instances=100,
+        shape=input_shape,
+        dtype='dataset')
+    y_train = common.generate_data(
+        num_instances=100,
+        shape=output_shape,
+        dtype='dataset')
+    dataset = tf.data.Dataset.zip(((x_train_1, x_train_2), y_train))
+
+    input_node1 = ak.Input(shape=input_shape)
+    temp_node1 = ak.Normalization()(input_node1)
+    output_node1 = ak.DenseBlock()(temp_node1)
+
+    output_node3 = ak.Normalization()(temp_node1)
+    output_node3 = ak.DenseBlock()(output_node3)
+
+    input_node2 = ak.Input(shape=input_shape)
+    output_node2 = ak.Normalization()(input_node2)
+    output_node2 = ak.DenseBlock()(output_node2)
+
+    output_node = ak.Merge()([output_node1, output_node2, output_node3])
+    output_node = ak.RegressionHead()(output_node)
+
+    graph = graph_module.HyperBuiltGraphHyperModel([input_node1, input_node2],
+                                                   output_node)
+    graph.preprocess(
+        hp=kerastuner.HyperParameters(),
+        dataset=dataset,
+        validation_data=dataset,
+        fit=True)
