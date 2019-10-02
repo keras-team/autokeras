781c8f2ac77933ed14f7c2f0436f95dceb4d9b38
Haifeng Jin <jhfjhfj1@gmail.com>
2019-10-02 14:04:56 -0500

    io api test

--- tests/integration_tests/io_api_test.py
+++ tests/integration_tests/io_api_test.py
@@ -1,46 +1,38 @@
-# import numpy as np
-# import autokeras as ak
-# import tensorflow as tf
-# import pytest
-# from tensorflow.python.keras.datasets import mnist
+import pytest
+from tensorflow.python.keras.datasets import mnist
+
+import autokeras as ak
+from tests import common
 
 
-# @pytest.fixture(scope='module')
-# def tmp_dir(tmpdir_factory):
-#     return tmpdir_factory.mktemp('test_io_api')
+@pytest.fixture(scope='module')
+def tmp_dir(tmpdir_factory):
+    return tmpdir_factory.mktemp('test_io_api')
 
 
-# def test_io_api(tmp_dir):
-#     (x_train, y_classification), (x_test, y_test) = mnist.load_data()
-#     data_slice = 20
-#     x_train = x_train[:data_slice]
-#     print(x_train.dtype)
-#     y_classification = y_classification[:data_slice]
-#     x_test = x_test[:data_slice]
-#     y_test = y_test[:data_slice]
-#     x_train = x_train.astype(np.float64)
-#     x_test = x_test.astype(np.float64)
-#     print(x_train.dtype)
-#     # x_image = np.reshape(x_train, (200, 28, 28, 1))
-#     # x_test = np.reshape(x_test, (200, 28, 28, 1))
-#     x_image = x_train.reshape(x_train.shape + (1,))
-#     x_test = x_test.reshape(x_test.shape + (1,))
-#     '''x_structured = np.random.rand(x_train.shape[0], 100)
-#     y_regression = np.random.rand(x_train.shape[0], 1)'''
-#     x_structured = np.random.rand(x_train.shape[0], 100)
-#     y_regression = np.random.rand(x_train.shape[0], 1)
-#     y_classification = y_classification.reshape(-1, 1)
-#     # y_classification = np.reshape(y_classification, (-1, 1))
-#     # Build model and train.
-#     automodel = ak.AutoModel(
-#         inputs=[ak.ImageInput(),
-#                 ak.StructuredDataInput()],
-#         outputs=[ak.RegressionHead(metrics=['mae']),
-#                  ak.ClassificationHead(loss='categorical_crossentropy',
-#                                        metrics=['accuracy'])],
-#         directory=tmp_dir,
-#         max_trials=3)
-#     automodel.fit([x_image, x_structured],
-#                   [y_regression, y_classification],
-#                   epochs=3,
-#                   validation_split=0.2)
+def test_io_api(tmp_dir):
+    (image_x, train_y), (test_x, test_y) = mnist.load_data()
+    (text_x, train_y), (test_x, test_y) = common.imdb_raw()
+
+    num_instances = 20
+    image_x = image_x[:num_instances]
+    text_x = text_x[:num_instances]
+    structured_data_x = common.generate_structured_data(num_instances=num_instances)
+    classification_y = common.generate_one_hot_labels(num_instances=num_instances,
+                                                      num_classes=2)
+    regression_y = common.generate_data(num_instances=num_instances, shape=(1,))
+
+    # Build model and train.
+    automodel = ak.AutoModel(
+        inputs=[ak.ImageInput(),
+                ak.TextInput(),
+                ak.StructuredDataInput()],
+        outputs=[ak.RegressionHead(metrics=['mae']),
+                 ak.ClassificationHead(loss='categorical_crossentropy',
+                                       metrics=['accuracy'])],
+        directory=tmp_dir,
+        max_trials=2)
+    automodel.fit([image_x, text_x, structured_data_x],
+                  [regression_y, classification_y],
+                  epochs=2,
+                  validation_split=0.2)
