784319d2901af4f9c5eab0110ab2d8841b1d5815
Haifeng Jin <jhfjhfj1@gmail.com>
2019-10-22 19:41:14 -0700

    cifar10

---
+++ examples/cifar10.py
@@ -0,0 +1,41 @@
+import os
+
+from tensorflow.keras.datasets import cifar10
+
+import autokeras as ak
+
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+def task_api():
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    clf = ak.ImageClassifier(seed=5, max_trials=10)
+    clf.fit(x_train, y_train, validation_split=0.2)
+    return clf.evaluate(x_test, y_test)
+
+
+def io_api():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    clf = ak.AutoModel(ak.ImageInput(),
+                       ak.ClassificationHead(),
+                       seed=5,
+                       max_trials=3)
+    clf.fit(x_train, y_train, validation_split=0.2)
+    return clf.evaluate(x_test, y_test)
+
+
+def functional_api():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    input_node = ak.ImageInput()
+    output_node = input_node
+    output_node = ak.Normalization()(output_node)
+    output_node = ak.ConvBlock()(output_node)
+    output_node = ak.SpatialReduction()(output_node)
+    output_node = ak.DenseBlock()(output_node)
+    output_node = ak.ClassificationHead()(output_node)
+    clf = ak.GraphAutoModel(input_node, output_node, seed=5, max_trials=3)
+    clf.fit(x_train, y_train, validation_split=0.2)
+    return clf.evaluate(x_test, y_test)
+
+
+if __name__ == '__main__':
+    task_api()
