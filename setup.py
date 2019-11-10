4898cab7d5a2be856dde7c8ca22ade67f883afb1
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-11-10 23:45:43 +0000

    Displaying the readme on the pypi page.

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from distutils.core import setup
-
 from setuptools import find_packages
+from pathlib import Path
+
+this_file = Path(__file__).resolve()
+readme = this_file.parent / 'README.md'
 
 setup(
     name='autokeras',
     version='1.0.0a0',
     description='AutoML for deep learning',
+    package_data={'': ['README.md']},
+    long_description=readme.read_text(encoding='utf-8'),
+    long_description_content_type='text/markdown',
     author='Data Analytics at Texas A&M (DATA) Lab, Keras Team',
     author_email='jhfjhfj1@gmail.com',
     url='http://autokeras.com',
