6485a8324cc5363ef4c0dc1302a0dcd06b101bbe
Haifeng Jin <jhfjhfj1@gmail.com>
2020-01-03 11:40:15 -0600

    implemented export model (#877)
    * implemented export model
    
    * bug fix
    
    * style fix

--- tests/autokeras/auto_model_test.py
+++ tests/autokeras/auto_model_test.py
@@ -106,3 +106,22 @@ def test_overwrite(tuner_fn, tmp_dir):
                               overwrite=False)
     auto_model.fit(x_train, y_train, epochs=2, validation_data=(x_train, y_train))
     assert not tuner_class.call_args_list[0][1]['overwrite']
+
+
+@mock.patch('autokeras.auto_model.tuner_module.get_tuner_class')
+def test_export_model(tuner_fn, tmp_dir):
+    tuner_class = tuner_fn.return_value
+    tuner = tuner_class.return_value
+    tuner.get_best_model.return_value = (mock.Mock(), mock.Mock())
+
+    x_train = np.random.rand(100, 32, 32, 3)
+    y_train = np.random.rand(100, 1)
+
+    auto_model = ak.AutoModel(ak.ImageInput(),
+                              ak.RegressionHead(),
+                              directory=tmp_dir,
+                              max_trials=2,
+                              overwrite=False)
+    auto_model.fit(x_train, y_train, epochs=2, validation_data=(x_train, y_train))
+    auto_model.export_model()
+    assert tuner.get_best_model.called
