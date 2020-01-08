b588858cd964eb0815261da070849be8e5c623e2
Haifeng Jin <jhfjhfj1@gmail.com>
2020-01-07 20:18:37 -0600

    script for updating website

--- shell/docs.sh
+++ shell/docs.sh
@@ -2,4 +2,13 @@
 cd docs
 python autogen.py
 mkdocs build
-echo "autokeras.com" > ../docs/site/CNAME
+cd ..
+echo "autokeras.com" > docs/site/CNAME
+git checkout -b gh-pages-temp
+git add -f docs/site
+git commit -m "gh-pages update"
+git subtree split --prefix docs/site -b gh-pages
+git push -f origin gh-pages:gh-pages
+git branch -D gh-pages
+git checkout master
+git branch -D gh-pages-temp
