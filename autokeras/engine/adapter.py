887f05c796040fd395644c8e23e87c4b228d2007
Haifeng Jin <jhfjhfj1@gmail.com>
2020-01-29 18:51:27 -0600

    docs

--- autokeras/engine/adapter.py
+++ autokeras/engine/adapter.py
@@ -6,25 +6,61 @@ from autokeras.engine import serializable
 
 
 class Adapter(serializable.Serializable):
-    """Adpat the input and output format for Keras Model."""
+    """Adpat the input and output format for Keras Model.
 
-    def __init__(self, shape=None, **kwargs):
+    Adapter is used by the input nodes and the heads of the hypermodel. It analyzes
+    the training data to get useful information, e.g., the shape of the data, which
+    is required for building the Keras Model. It also converts the dataset to
+    tf.data.Dataset format.
+
+    # Arguments
+        shape: Tuple of int. The input or output shape of the hypermodel.
+    """
+
+    def __init__(self, shape=None):
         self.shape = shape
 
     def check(self, dataset):
-        pass
+        """Check if the dataset is valid for the input node.
 
-    def convert_to_dataset(self, x):
-        if isinstance(x, tf.data.Dataset):
-            return x
-        if isinstance(x, np.ndarray):
-            x = x.astype(np.float32)
-            return tf.data.Dataset.from_tensor_slices(x)
+        # Arguments
+            dataset: Usually numpy.ndarray, pandas.DataFrame or tensorflow.Dataset.
+                The dataset to be checked.
+
+        # Returns
+            Boolean. Whether the dataset is valid for the input node.
+        """
+        return True
+
+    def convert_to_dataset(self, dataset):
+        """Convert supported formats of datasets to tf.data.Dataset.
+
+        # Arguments
+            dataset: Usually numpy.ndarray, pandas.DataFrame or tensorflow.Dataset.
+                The dataset to be converted.
+
+        # Returns
+            tf.data.Dataset. The converted dataset.
+        """
+        if isinstance(dataset, tf.data.Dataset):
+            return dataset
+        if isinstance(dataset, np.ndarray):
+            return tf.data.Dataset.from_tensor_slices(dataset.astype(np.float32))
 
     def fit(self, dataset):
+        """Analyze the dataset and record useful information.
+
+        # Arguments
+            dataset: tf.data.Dataset.
+        """
         self.record_dataset_shape(dataset)
 
     def fit_before_convert(self, dataset):
+        """Analyze the dataset before converting to tf.data.Dataset.
+
+        # Arguments
+            dataset: Usually numpy.ndarray, pandas.DataFrame or tensorflow.Dataset.
+        """
         pass
 
     def fit_transform(self, dataset):
@@ -38,6 +74,15 @@ class Adapter(serializable.Serializable):
         self.shape = utils.dataset_shape(dataset)
 
     def transform(self, dataset):
+        """Transform the input dataset to tf.data.Dataset.
+
+        # Arguments
+            dataset: Usually numpy.ndarray, pandas.DataFrame or tensorflow.Dataset.
+                The dataset to be transformed.
+
+        # Returns
+            tf.data.Dataset. The converted dataset.
+        """
         self.check(dataset)
         return self.convert_to_dataset(dataset)
 
