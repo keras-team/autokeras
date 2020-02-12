5a29b24caee11ba58b51327b29aa7037e0cb1b4d
Gabriel de Marmiesse <gabrieldemarmiesse@gmail.com>
2020-02-12 20:33:48 +0100

    Making a pre-commit with docker (#955)
    * Added pre-commit running docker.
    
    * Remove verbose.
    
    * Let's first take care of isort.
    
    * Tried with command line arg.
    
    * Hacking.
    
    * Come back to the previous behavior.
    
    * Some progress.
    
    * Tried something.
    
    * Some progress.
    
    * Finally works.
    
    * Enabled autopep8
    
    * Finished pre-commit.
    
    * Added instructions.
    
    * Update CONTRIBUTING.md
    
    Co-authored-by: Haifeng Jin <jhfjhfj1@gmail.com>

---
+++ docker/pre_commit.py
@@ -0,0 +1,50 @@
+from subprocess import CalledProcessError
+from subprocess import check_call
+
+
+def check_bash_call(string):
+    check_call(["bash", "-c", string])
+
+
+def _run_format_and_flake8():
+    files_changed = False
+
+    try:
+        check_bash_call("isort -sl -rc -c")
+    except CalledProcessError:
+        check_bash_call("isort -y -sl -rc")
+        files_changed = True
+
+    try:
+        check_bash_call("autopep8 --exit-code -i -r .")
+    except CalledProcessError as error:
+        if error.returncode == 2:
+            files_changed = True
+        else:
+            # there was another type of error
+            raise
+
+    if files_changed:
+        print("Some files have changed.")
+        print("Please do git add and git commit again")
+    else:
+        print("No formatting needed.")
+
+    print("Running flake8.")
+    check_bash_call("flake8")
+    print("Done")
+
+    if files_changed:
+        exit(1)
+
+
+def run_format_and_flake8():
+    try:
+        _run_format_and_flake8()
+    except CalledProcessError as error:
+        print("Pre-commit returned exit code", error.returncode)
+        exit(error.returncode)
+
+
+if __name__ == "__main__":
+    run_format_and_flake8()
