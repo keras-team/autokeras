ee5be5a29e145eb496a67843e7ce86f7f99b4529
Kuangshi <cnkuangshi@gmail.com>
2020-03-21 03:28:46 +0800

    Add type restriction on RegressionHead (#1053)
    Add type restriction on RegressionHead init function

--- autokeras/hypermodels/heads.py
+++ autokeras/hypermodels/heads.py
@@ -131,7 +131,7 @@ class RegressionHead(head_module.Head):
                  output_dim: Optional[int] = None,
                  loss: types.LossType = 'mean_squared_error',
                  metrics: Optional[types.MetricsType] = None,
-                 dropout_rate=None,
+                 dropout_rate: Optional[float] = None,
                  **kwargs):
         super().__init__(loss=loss,
                          metrics=metrics,
