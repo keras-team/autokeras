6ed3d93e84b95e4f00c0c2e8174b59378508cde8
Haifeng Jin <jhfjhfj1@gmail.com>
2020-06-16 07:01:40 +0000

    revised blocks tests

--- tests/autokeras/blocks/basic_test.py
+++ tests/autokeras/blocks/basic_test.py
@@ -1,98 +1,204 @@
+import kerastuner
 import pytest
 import tensorflow as tf
+from tensorflow.python.util import nest
 
+from autokeras import blocks
 from autokeras.blocks import basic
-from tests.autokeras.blocks import utils
+from tests import utils
 
 
-def test_type_error_for_call():
-    block = basic.ConvBlock()
-    with pytest.raises(TypeError) as info:
-        block(block)
-    assert 'Expect the inputs to layer' in str(info.value)
+def test_resnet_build_return_tensor():
+    block = blocks.ResNetBlock()
+
+    outputs = block.build(
+        kerastuner.HyperParameters(),
+        tf.keras.Input(shape=(32, 32, 3), dtype=tf.float32))
+
+    assert len(nest.flatten(outputs)) == 1
+    assert isinstance(nest.flatten(outputs)[0], tf.Tensor)
 
 
-def test_resnet_block():
-    utils.block_basic_exam(
-        basic.ResNetBlock(),
-        tf.keras.Input(shape=(32, 32, 3), dtype=tf.float32),
-        ['version', 'pooling'],
-    )
+def test_resnet_deserialize_to_resnet():
+    serialized_block = blocks.serialize(blocks.ResNetBlock())
+
+    block = blocks.deserialize(serialized_block)
+
+    assert isinstance(block, blocks.ResNetBlock)
 
 
-def test_resnet_invalid_kwargs():
+def test_resnet_get_config_has_all_attributes():
+    block = blocks.ResNetBlock()
+
+    config = block.get_config()
+
+    assert utils.get_func_args(
+        blocks.ResNetBlock.__init__).issubset(config.keys())
+
+
+def test_resnet_init_error_with_include_top():
     with pytest.raises(ValueError) as info:
-        basic.ResNetBlock(include_top=True)
+        blocks.ResNetBlock(include_top=True)
+
     assert 'Argument "include_top" is not' in str(info.value)
 
 
-def test_resnet_invalid_kwargs2():
+def test_resnet_init_error_with_input_shape():
     with pytest.raises(ValueError) as info:
-        basic.ResNetBlock(input_shape=(10,))
+        blocks.ResNetBlock(input_shape=(10,))
+
     assert 'Argument "input_shape" is not' in str(info.value)
 
 
-def test_xception_block():
-    utils.block_basic_exam(
-        basic.XceptionBlock(),
-        tf.keras.Input(shape=(32, 32, 3), dtype=tf.float32),
-        [
-            'activation',
-            'initial_strides',
-            'num_residual_blocks',
-            'pooling',
-        ])
+def test_xception_build_return_tensor():
+    block = blocks.XceptionBlock()
+
+    outputs = block.build(
+        kerastuner.HyperParameters(),
+        tf.keras.Input(shape=(32, 32, 3), dtype=tf.float32))
+
+    assert len(nest.flatten(outputs)) == 1
+    assert isinstance(nest.flatten(outputs)[0], tf.Tensor)
 
 
-def test_xception_invalid_kwargs():
+def test_xception_deserialize_to_xception():
+    serialized_block = blocks.serialize(blocks.XceptionBlock())
+
+    block = blocks.deserialize(serialized_block)
+
+    assert isinstance(block, blocks.XceptionBlock)
+
+
+def test_xception_get_config_has_all_attributes():
+    block = blocks.XceptionBlock()
+
+    config = block.get_config()
+
+    assert utils.get_func_args(
+        blocks.XceptionBlock.__init__).issubset(config.keys())
+
+
+def test_xception_init_error_with_include_top():
     with pytest.raises(ValueError) as info:
         basic.XceptionBlock(include_top=True)
+
     assert 'Argument "include_top" is not' in str(info.value)
 
 
-def test_xception_invalid_kwargs2():
+def test_xception_init_error_with_input_shape():
     with pytest.raises(ValueError) as info:
         basic.XceptionBlock(input_shape=(10,))
+
     assert 'Argument "input_shape" is not' in str(info.value)
 
 
-def test_conv_block():
-    utils.block_basic_exam(
-        basic.ConvBlock(),
-        tf.keras.Input(shape=(32, 32, 3), dtype=tf.float32),
-        [
-            'kernel_size',
-            'num_blocks',
-            'separable',
-        ])
+def test_conv_build_return_tensor():
+    block = blocks.ConvBlock()
+
+    outputs = block.build(
+        kerastuner.HyperParameters(),
+        tf.keras.Input(shape=(32, 32, 3), dtype=tf.float32))
+
+    assert len(nest.flatten(outputs)) == 1
+    assert isinstance(nest.flatten(outputs)[0], tf.Tensor)
 
 
-def test_rnn_block():
-    utils.block_basic_exam(
-        basic.RNNBlock(),
-        tf.keras.Input(shape=(32, 10), dtype=tf.float32),
-        [
-            'bidirectional',
-            'layer_type',
-            'num_layers',
-        ])
+def test_conv_deserialize_to_conv():
+    serialized_block = blocks.serialize(blocks.ConvBlock())
+
+    block = blocks.deserialize(serialized_block)
+
+    assert isinstance(block, blocks.ConvBlock)
 
 
-def test_dense_block():
-    utils.block_basic_exam(
-        basic.DenseBlock(),
-        tf.keras.Input(shape=(32,), dtype=tf.float32),
-        [
-            'num_layers',
-            'use_batchnorm',
-        ])
+def test_conv_get_config_has_all_attributes():
+    block = blocks.ConvBlock()
+
+    config = block.get_config()
+
+    assert utils.get_func_args(
+        blocks.ConvBlock.__init__).issubset(config.keys())
 
 
-def test_embedding_block():
-    utils.block_basic_exam(
-        basic.Embedding(),
-        tf.keras.Input(shape=(32,), dtype=tf.float32),
-        [
-            'pretraining',
-            'embedding_dim',
-        ])
+def test_rnn_build_return_tensor():
+    block = blocks.RNNBlock()
+
+    outputs = block.build(
+        kerastuner.HyperParameters(),
+        tf.keras.Input(shape=(32, 10), dtype=tf.float32))
+
+    assert len(nest.flatten(outputs)) == 1
+    assert isinstance(nest.flatten(outputs)[0], tf.Tensor)
+
+
+def test_rnn_deserialize_to_rnn():
+    serialized_block = blocks.serialize(blocks.RNNBlock())
+
+    block = blocks.deserialize(serialized_block)
+
+    assert isinstance(block, blocks.RNNBlock)
+
+
+def test_rnn_get_config_has_all_attributes():
+    block = blocks.RNNBlock()
+
+    config = block.get_config()
+
+    assert utils.get_func_args(
+        blocks.RNNBlock.__init__).issubset(config.keys())
+
+
+def test_dense_build_return_tensor():
+    block = blocks.DenseBlock()
+
+    outputs = block.build(
+        kerastuner.HyperParameters(),
+        tf.keras.Input(shape=(32,), dtype=tf.float32))
+
+    assert len(nest.flatten(outputs)) == 1
+    assert isinstance(nest.flatten(outputs)[0], tf.Tensor)
+
+
+def test_dense_deserialize_to_dense():
+    serialized_block = blocks.serialize(blocks.DenseBlock())
+
+    block = blocks.deserialize(serialized_block)
+
+    assert isinstance(block, blocks.DenseBlock)
+
+
+def test_dense_get_config_has_all_attributes():
+    block = blocks.DenseBlock()
+
+    config = block.get_config()
+
+    assert utils.get_func_args(
+        blocks.DenseBlock.__init__).issubset(config.keys())
+
+
+def test_embed_build_return_tensor():
+    block = blocks.Embedding()
+
+    outputs = block.build(
+        kerastuner.HyperParameters(),
+        tf.keras.Input(shape=(32,), dtype=tf.float32))
+
+    assert len(nest.flatten(outputs)) == 1
+    assert isinstance(nest.flatten(outputs)[0], tf.Tensor)
+
+
+def test_embed_deserialize_to_embed():
+    serialized_block = blocks.serialize(blocks.Embedding())
+
+    block = blocks.deserialize(serialized_block)
+
+    assert isinstance(block, blocks.Embedding)
+
+
+def test_embed_get_config_has_all_attributes():
+    block = blocks.Embedding()
+
+    config = block.get_config()
+
+    assert utils.get_func_args(
+        blocks.Embedding.__init__).issubset(config.keys())
--- tests/autokeras/blocks/preprocessing_test.py
+++ tests/autokeras/blocks/preprocessing_test.py
@@ -1,39 +1,120 @@
+import kerastuner
 import tensorflow as tf
+from tensorflow.python.util import nest
 
-from autokeras.blocks import preprocessing
-from tests.autokeras.blocks import utils
-
-
-def test_image_augmentation():
-    utils.block_basic_exam(
-        preprocessing.ImageAugmentation(),
-        tf.keras.Input(shape=(32, 32, 3), dtype=tf.float32),
-        ['vertical_flip', 'horizontal_flip'],
-    )
+from autokeras import blocks
+from tests import utils
 
 
-def test_text_to_ngram_vector():
-    utils.block_basic_exam(
-        preprocessing.TextToNgramVector(),
-        tf.keras.Input(shape=(1,), dtype=tf.string),
-        ['ngrams'],
-    )
+def test_augment_build_return_tensor():
+    block = blocks.ImageAugmentation()
+
+    outputs = block.build(
+        kerastuner.HyperParameters(),
+        tf.keras.Input(shape=(32, 32, 3), dtype=tf.float32))
+
+    assert len(nest.flatten(outputs)) == 1
+    assert isinstance(nest.flatten(outputs)[0], tf.Tensor)
 
 
-def test_text_to_int_sequence():
-    utils.block_basic_exam(
-        preprocessing.TextToIntSequence(),
-        tf.keras.Input(shape=(1,), dtype=tf.string),
-        ['output_sequence_length'],
-    )
+def test_augment_deserialize_to_augment():
+    serialized_block = blocks.serialize(blocks.ImageAugmentation())
+
+    block = blocks.deserialize(serialized_block)
+
+    assert isinstance(block, blocks.ImageAugmentation)
 
 
-def test_categorical_to_numerical():
-    block = preprocessing.CategoricalToNumerical()
+def test_augment_get_config_has_all_attributes():
+    block = blocks.ImageAugmentation()
+
+    config = block.get_config()
+
+    assert utils.get_func_args(
+        blocks.ImageAugmentation.__init__).issubset(config.keys())
+
+
+def test_ngram_build_return_tensor():
+    block = blocks.TextToNgramVector()
+
+    outputs = block.build(
+        kerastuner.HyperParameters(),
+        tf.keras.Input(shape=(1,), dtype=tf.string))
+
+    assert len(nest.flatten(outputs)) == 1
+    assert isinstance(nest.flatten(outputs)[0], tf.Tensor)
+
+
+def test_ngram_deserialize_to_ngram():
+    serialized_block = blocks.serialize(blocks.TextToNgramVector())
+
+    block = blocks.deserialize(serialized_block)
+
+    assert isinstance(block, blocks.TextToNgramVector)
+
+
+def test_ngram_get_config_has_all_attributes():
+    block = blocks.TextToNgramVector()
+
+    config = block.get_config()
+
+    assert utils.get_func_args(
+        blocks.TextToNgramVector.__init__).issubset(config.keys())
+
+
+def test_int_seq_build_return_tensor():
+    block = blocks.TextToIntSequence()
+
+    outputs = block.build(
+        kerastuner.HyperParameters(),
+        tf.keras.Input(shape=(1,), dtype=tf.string))
+
+    assert len(nest.flatten(outputs)) == 1
+    assert isinstance(nest.flatten(outputs)[0], tf.Tensor)
+
+
+def test_int_seq_deserialize_to_int_seq():
+    serialized_block = blocks.serialize(blocks.TextToIntSequence())
+
+    block = blocks.deserialize(serialized_block)
+
+    assert isinstance(block, blocks.TextToIntSequence)
+
+
+def test_int_seq_get_config_has_all_attributes():
+    block = blocks.TextToIntSequence()
+
+    config = block.get_config()
+
+    assert utils.get_func_args(
+        blocks.TextToIntSequence.__init__).issubset(config.keys())
+
+
+def test_cat_to_num_build_return_tensor():
+    block = blocks.CategoricalToNumerical()
     block.column_names = ['a']
     block.column_types = {'a': 'num'}
-    utils.block_basic_exam(
-        block,
-        tf.keras.Input(shape=(1,), dtype=tf.string),
-        [],
-    )
+
+    outputs = block.build(
+        kerastuner.HyperParameters(),
+        tf.keras.Input(shape=(1,), dtype=tf.string))
+
+    assert len(nest.flatten(outputs)) == 1
+    assert isinstance(nest.flatten(outputs)[0], tf.Tensor)
+
+
+def test_cat_to_num_deserialize_to_cat_to_num():
+    serialized_block = blocks.serialize(blocks.CategoricalToNumerical())
+
+    block = blocks.deserialize(serialized_block)
+
+    assert isinstance(block, blocks.CategoricalToNumerical)
+
+
+def test_cat_to_num_get_config_has_all_attributes():
+    block = blocks.CategoricalToNumerical()
+
+    config = block.get_config()
+
+    assert utils.get_func_args(
+        blocks.CategoricalToNumerical.__init__).issubset(config.keys())
