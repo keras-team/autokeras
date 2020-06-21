149d0b0d0fdd8ebe15f3a0b37e1678dd4fa84b7f
Haifeng Jin <haifengj@google.com>
2020-06-21 04:58:45 +0000

    revised task api tests

--- tests/autokeras/tasks/text_test.py
+++ tests/autokeras/tasks/text_test.py
@@ -1,16 +1,28 @@
 from unittest import mock
 
-from autokeras.tasks import text
+import numpy as np
+
+import autokeras as ak
 from tests import utils
 
 
-@mock.patch('autokeras.auto_model.AutoModel.__init__')
-def test_text_classifier(auto_model, tmp_path):
-    text.TextClassifier(directory=tmp_path, max_trials=2, seed=utils.SEED)
-    assert auto_model.called
+@mock.patch('autokeras.AutoModel.fit')
+def test_txt_clf_fit_call_auto_model_fit(fit, tmp_path):
+    auto_model = ak.TextClassifier(directory=tmp_path, seed=utils.SEED)
+
+    auto_model.fit(
+        x=np.array(['a b c', 'b b c']),
+        y=np.array([1, 2]))
+
+    assert fit.is_called
 
 
-@mock.patch('autokeras.auto_model.AutoModel.__init__')
-def test_text_regressor(auto_model, tmp_path):
-    text.TextRegressor(directory=tmp_path, max_trials=2, seed=utils.SEED)
-    assert auto_model.called
+@mock.patch('autokeras.AutoModel.fit')
+def test_txt_reg_fit_call_auto_model_fit(fit, tmp_path):
+    auto_model = ak.TextRegressor(directory=tmp_path, seed=utils.SEED)
+
+    auto_model.fit(
+        x=np.array(['a b c', 'b b c']),
+        y=np.array([1.0, 2.0]))
+
+    assert fit.is_called
