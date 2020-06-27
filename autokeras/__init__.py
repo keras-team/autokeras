7b75f34b3f8364903719733a2eb9dd8ca9069e20
Haifeng Jin <haifengj@google.com>
2020-06-27 09:08:36 +0000

    bump tf version to 2.3.0

--- autokeras/__init__.py
+++ autokeras/__init__.py
@@ -23,7 +23,7 @@ from autokeras.blocks import XceptionBlock
 from autokeras.engine.block import Block
 from autokeras.engine.head import Head
 from autokeras.engine.node import Node
-from autokeras.keras_layers import CUSTOM_OBJECTS
+from autokeras.keras_layers import MultiColumnCategoricalEncoding
 from autokeras.nodes import ImageInput
 from autokeras.nodes import Input
 from autokeras.nodes import StructuredDataInput
@@ -47,3 +47,7 @@ __version__ = '1.0.5dev'
 
 check_tf_version()
 check_kt_version()
+
+CUSTOM_OBJECTS = {
+    'MultiColumnCategoricalEncoding': MultiColumnCategoricalEncoding,
+}
