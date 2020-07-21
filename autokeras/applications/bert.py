f7c689ad525833b15d248f641d8060c185a11737
Ankur Kunder <a.kunder@tamu.edu>
2020-07-21 11:52:39 -0500

    added missing dependencies

--- autokeras/applications/bert.py
+++ autokeras/applications/bert.py
@@ -193,6 +193,17 @@ import os
 # import tensorflow_hub as hub
 from official.nlp import bert
 
+# Load the required submodules
+import official.nlp.optimization
+import official.nlp.bert.bert_models
+import official.nlp.bert.configs
+import official.nlp.bert.run_classifier
+import official.nlp.bert.tokenization
+import official.nlp.data.classifier_data_lib
+import official.nlp.modeling.losses
+import official.nlp.modeling.models
+import official.nlp.modeling.networks
+
 gs_folder_bert = "gs://cloud-tpu-checkpoints/bert/keras_bert/uncased_L-12_H-768_A-12"
 # tf.io.gfile.listdir(gs_folder_bert)
 hub_url_bert = "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/2"
