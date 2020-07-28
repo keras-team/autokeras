be62644a51c6fdb5c98ba7af4c9882c8cb42f3ef
Haifeng Jin <jin@tamu.edu>
2020-07-28 18:36:55 +0000

    add copyright to lint and format

--- shell/format.sh
+++ shell/format.sh
@@ -1,3 +1,11 @@
-isort -sl -rc autokeras tests
+isort --sl autokeras tests
 autopep8 -r -i autokeras tests
 
+for i in $(find autokeras tests -name '*.py') # or whatever other pattern...
+do
+  if ! grep -q Copyright $i
+  then
+    echo $i
+    cat copyright.txt $i >$i.new && mv $i.new $i
+  fi
+done
