0767d553543c1365b5591b6b8fe11326557429a1
Ankur Kunder <a.kunder@tamu.edu>
2020-08-03 19:07:17 -0500

    "test 1"

--- autokeras/keras_layers.py
+++ autokeras/keras_layers.py
@@ -114,7 +114,7 @@ class TextVectorizationWithTokenizer(preprocessing.PreprocessingLayer):
     def call(self, inputs):
         print("Tokenizer call function: ", inputs.shape)
         # return self.bert_encode(inputs)
-        output = tf.numpy_function(func=self.bert_encode, inp=[inputs], Tout=(tf.int32, tf.int32, tf.int32))
+        output = tf.numpy_function(func=self.bert_encode, inp=[inputs], Tout=[tf.int32, tf.int32, tf.int32])
         # output.set_shape([None, None, None])
         return output
 
@@ -167,5 +167,6 @@ class TextVectorizationWithTokenizer(preprocessing.PreprocessingLayer):
         #     'input_mask': input_mask,
         #     'input_type_ids': input_type_ids}
         inputs = [input_word_ids.numpy(),input_mask, input_type_ids]
-
+        print('bert_encode output len: ', len(inputs))
+        print('bert_encode input_word_ids shape: ', input_word_ids.shape)
         return inputs
