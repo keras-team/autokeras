98ca122745510e821fe4480c7a5b7d28e827814b
Ankur Kunder <a.kunder@tamu.edu>
2020-08-23 18:22:21 -0500

    "rebased onto master"

--- tests/autokeras/tuners/task_specific_test.py
+++ tests/autokeras/tuners/task_specific_test.py
@@ -77,7 +77,7 @@ def test_txt_clf_init_hp0_equals_hp_of_a_model(clear_session, tmp_path):
     hp.values = copy.copy(init_hp)
 
     clf.tuner.hypermodel.build(hp)
-
+    print("TEXT_CLASSIFIER hp: ", set(init_hp.keys()), "TextClassifier hp: ", set(hp._hps.keys()))
     assert set(init_hp.keys()) == set(hp._hps.keys())
 
 
@@ -86,6 +86,19 @@ def test_txt_clf_init_hp1_equals_hp_of_a_model(clear_session, tmp_path):
     clf.inputs[0].shape = (1,)
     clf.outputs[0].in_blocks[0].output_shape = (10,)
     init_hp = task_specific.TEXT_CLASSIFIER[1]
+    hp = kerastuner.HyperParameters()
+    hp.values = copy.copy(init_hp)
+
+    clf.tuner.hypermodel.build(hp)
+
+    assert set(init_hp.keys()) == set(hp._hps.keys())
+
+
+def test_txt_clf_init_hp2_equals_hp_of_a_model(clear_session, tmp_path):
+    clf = ak.TextClassifier(directory=tmp_path)
+    clf.inputs[0].shape = (1,)
+    clf.outputs[0].in_blocks[0].output_shape = (10,)
+    init_hp = task_specific.TEXT_CLASSIFIER[2]
     hp = kerastuner.HyperParameters()
     hp.values = copy.copy(init_hp)
 
