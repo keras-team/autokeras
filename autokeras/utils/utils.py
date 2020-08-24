67cc7b5915b6e35564027c7a1ee4d09c9c57de60
Ankur Kunder <a.kunder@tamu.edu>
2020-08-24 02:44:45 -0500

    fixing the error message from 1.0.2rc0 to 1.0.2rc1 (#1305)
    * "fixing the error message from 1.0.2rc0 to 1.0.2rc1"
    
    * "fixing the error message from 1.0.2rc0 to 1.0.2rc1"

--- autokeras/utils/utils.py
+++ autokeras/utils/utils.py
@@ -49,14 +49,14 @@ def check_tf_version() -> None:
 
 
 def check_kt_version() -> None:
-    if parse(kerastuner.__version__) < parse("1.0.2rc0"):
+    if parse(kerastuner.__version__) < parse("1.0.2rc1"):
         raise ImportError(
-            "The Keras Tuner package version needs to be at least 1.0.2rc0 \n"
+            "The Keras Tuner package version needs to be at least 1.0.2rc1 \n"
             "for AutoKeras to run. Currently, your Keras Tuner version is \n"
             "{version}. Please upgrade with \n"
             "`$ pip install "
-            "git+https://github.com/keras-team/keras-tuner.git@1.0.2rc0"
-            "#egg=keras-tuner-1.0.2rc0`. \n"
+            "git+https://github.com/keras-team/keras-tuner.git@1.0.2rc1"
+            "#egg=keras-tuner-1.0.2rc1`. \n"
             "You can use `pip freeze` to check afterwards that everything is "
             "ok.".format(version=kerastuner.__version__)
         )
