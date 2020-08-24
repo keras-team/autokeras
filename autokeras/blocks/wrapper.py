3ebcf935272eac8ed6be0c1e1d58cf5dc726f613
Haifeng Jin <jhfjhfj1@gmail.com>
2020-08-24 18:18:25 -0500

    add normalize to structured data block

--- autokeras/blocks/wrapper.py
+++ autokeras/blocks/wrapper.py
@@ -181,14 +181,21 @@ class StructuredDataBlock(block_module.Block):
     # Arguments
         categorical_encoding: Boolean. Whether to use the CategoricalToNumerical to
             encode the categorical features to numerical features. Defaults to True.
+        normalize: Boolean. Whether to normalize the features.
+            If unspecified, it will be tuned automatically.
         seed: Int. Random seed.
     """
 
     def __init__(
-        self, categorical_encoding: bool = True, seed: Optional[int] = None, **kwargs
+        self,
+        categorical_encoding: bool = True,
+        normalize: Optional[bool] = None,
+        seed: Optional[int] = None,
+        **kwargs
     ):
         super().__init__(**kwargs)
         self.categorical_encoding = categorical_encoding
+        self.normalize = normalize
         self.seed = seed
         self.column_types = None
         self.column_names = None
@@ -207,6 +214,7 @@ class StructuredDataBlock(block_module.Block):
         config.update(
             {
                 "categorical_encoding": self.categorical_encoding,
+                "normalize": self.normalize,
                 "seed": self.seed,
                 "column_types": self.column_types,
                 "column_names": self.column_names,
@@ -222,6 +230,13 @@ class StructuredDataBlock(block_module.Block):
             block.column_types = self.column_types
             block.column_names = self.column_names
             output_node = block.build(hp, output_node)
+
+        if self.normalize is None and hp.Boolean(NORMALIZE):
+            with hp.conditional_scope(NORMALIZE, [True]):
+                output_node = preprocessing.Normalization().build(hp, output_node)
+        elif self.normalize:
+            output_node = preprocessing.Normalization().build(hp, output_node)
+
         output_node = basic.DenseBlock().build(hp, output_node)
         return output_node
 
