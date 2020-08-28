87f9703502e7137619d00959a5e03014cdd8a6aa
Fayaz Rahman <fayazrahman4u@gmail.com>
2020-08-28 16:19:40 +0530

    Update utils.py

--- tests/utils.py
+++ tests/utils.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import inspect
+import os
 
 import numpy as np
 import tensorflow as tf
