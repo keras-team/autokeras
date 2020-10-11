0aa9a4ae159a3e81b3bc804286f35207abb758a3
Abraham Sebastian <abraham.g.sebastian@gmail.com>
2020-10-11 10:17:45 -0500

    Added way to include Trainable Preprocessing layer as Hyper Preprocessor, also implemented CategoricalToNumerical using Preprocessor base class

--- tests/autokeras/preprocessors/common_test.py
+++ tests/autokeras/preprocessors/common_test.py
@@ -24,3 +24,26 @@ def test_time_series_input_transform():
     preprocessor = common.SlidingWindow(lookback=2, batch_size=32)
     x = preprocessor.transform(dataset)
     assert data_utils.dataset_shape(x).as_list() == [None, 2, 32]
+
+
+def test_categorical_to_numerical_input_transform():
+    x_train = np.array([["a", "ab", 2.1], ["b", "bc", 1.0], ["a", "bc", "nan"]])
+    preprocessor = common.CategoricalToNumerical(
+        column_names=["column_a", "column_b", "column_c"],
+        column_types={
+            "column_a": "categorical",
+            "column_b": "categorical",
+            "column_c": "numerical",
+        },
+    )
+    dataset = tf.data.Dataset.from_tensor_slices(x_train).batch(32)
+
+    preprocessor.fit(tf.data.Dataset.from_tensor_slices(x_train).batch(32))
+    result = preprocessor.transform(dataset)
+
+    assert result[0][0] == result[2][0]
+    assert result[0][0] != result[1][0]
+    assert result[0][1] != result[1][1]
+    assert result[0][1] != result[2][1]
+    assert result[2][2] == 0
+    assert result.dtype == tf.float32
