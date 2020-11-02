90246feb0d73e84a32bea3a0736e60737e572e18
Ankur Kunder <a.kunder@tamu.edu>
2020-11-02 14:53:54 -0600

    "rebased onto master"

--- autokeras/keras_layers.py
+++ autokeras/keras_layers.py
@@ -1750,17 +1750,17 @@ class ObjectDetectionPreProcessing(preprocessing.PreprocessingLayer):
         max_sequence_length: maximum length of the sequences after vectorization.
     """
 
-    def __init__(self, max_sequence_length, **kwargs):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.tokenizer = tokenization.FullTokenizer(
-            vocab_file=constants.BERT_VOCAB_PATH,
-            do_lower_case=True,
-        )
-        self.max_sequence_length = max_sequence_length
+        # self.tokenizer = tokenization.FullTokenizer(
+        #     # vocab_file=constants.BERT_VOCAB_PATH,
+        #     do_lower_case=True,
+        # )
+        # self.max_sequence_length = max_sequence_length
 
     def get_config(self):
         config = super().get_config()
-        config.update({"max_sequence_length": self.max_sequence_length})
+        # config.update({"max_sequence_length": self.max_sequence_length})
         return config
 
     def build(self, input_shape):
@@ -2050,6 +2050,69 @@ class AnchorBox:
         return tf.concat(anchors, axis=0)
 
 
+class RetinaNet(tf.keras.Model):
+    """A subclassed Keras model implementing the RetinaNet architecture.
+
+    Attributes:
+      num_classes: Number of classes in the dataset.
+      backbone: The backbone to build the feature pyramid from.
+        Currently supports ResNet50 only.
+    """
+
+    def __init__(self, num_classes, backbone=None, **kwargs):
+        super(RetinaNet, self).__init__(name="RetinaNet", **kwargs)
+        self.fpn = FeaturePyramid(backbone)
+        self.num_classes = num_classes
+
+        prior_probability = tf.constant_initializer(-np.log((1 - 0.01) / 0.01))
+        self.cls_head = build_head(9 * num_classes, prior_probability)
+        self.box_head = build_head(9 * 4, "zeros")
+
+    def call(self, image, training=False):
+        features = self.fpn(image, training=training)
+        N = tf.shape(image)[0]
+        cls_outputs = []
+        box_outputs = []
+        for feature in features:
+            box_outputs.append(tf.reshape(self.box_head(feature), [N, -1, 4]))
+            cls_outputs.append(
+                tf.reshape(self.cls_head(feature), [N, -1, self.num_classes])
+            )
+        cls_outputs = tf.concat(cls_outputs, axis=1)
+        box_outputs = tf.concat(box_outputs, axis=1)
+        return tf.concat([box_outputs, cls_outputs], axis=-1)
+
+def build_head(output_filters, bias_init):
+    """Builds the class/box predictions head.
+
+    Arguments:
+      output_filters: Number of convolution filters in the final layer.
+      bias_init: Bias Initializer for the final convolution layer.
+
+    Returns:
+      A keras sequential model representing either the classification
+        or the box regression head depending on `output_filters`.
+    """
+    head = tf.keras.Sequential([tf.keras.Input(shape=[None, None, 256])])
+    kernel_init = tf.initializers.RandomNormal(0.0, 0.01)
+    for _ in range(4):
+        head.add(
+            tf.keras.layers.Conv2D(256, 3, padding="same", kernel_initializer=kernel_init)
+        )
+        head.add(tf.keras.layers.ReLU())
+    head.add(
+        tf.keras.layers.Conv2D(
+            output_filters,
+            3,
+            1,
+            padding="same",
+            kernel_initializer=kernel_init,
+            bias_initializer=bias_init,
+        )
+    )
+    return head
+
+
 class LabelEncoder:
     """Transforms the raw labels into targets for training.
 
@@ -2181,66 +2244,3 @@ class LabelEncoder:
             labels = labels.write(i, label)
         batch_images = tf.keras.applications.resnet.preprocess_input(batch_images)  ## TODO
         return batch_images, labels.stack()
-
-
-class RetinaNet(tf.keras.Model):
-    """A subclassed Keras model implementing the RetinaNet architecture.
-
-    Attributes:
-      num_classes: Number of classes in the dataset.
-      backbone: The backbone to build the feature pyramid from.
-        Currently supports ResNet50 only.
-    """
-
-    def __init__(self, num_classes, backbone=None, **kwargs):
-        super(RetinaNet, self).__init__(name="RetinaNet", **kwargs)
-        self.fpn = FeaturePyramid(backbone)
-        self.num_classes = num_classes
-
-        prior_probability = tf.constant_initializer(-np.log((1 - 0.01) / 0.01))
-        self.cls_head = build_head(9 * num_classes, prior_probability)
-        self.box_head = build_head(9 * 4, "zeros")
-
-    def call(self, image, training=False):
-        features = self.fpn(image, training=training)
-        N = tf.shape(image)[0]
-        cls_outputs = []
-        box_outputs = []
-        for feature in features:
-            box_outputs.append(tf.reshape(self.box_head(feature), [N, -1, 4]))
-            cls_outputs.append(
-                tf.reshape(self.cls_head(feature), [N, -1, self.num_classes])
-            )
-        cls_outputs = tf.concat(cls_outputs, axis=1)
-        box_outputs = tf.concat(box_outputs, axis=1)
-        return tf.concat([box_outputs, cls_outputs], axis=-1)
-
-def build_head(output_filters, bias_init):
-    """Builds the class/box predictions head.
-
-    Arguments:
-      output_filters: Number of convolution filters in the final layer.
-      bias_init: Bias Initializer for the final convolution layer.
-
-    Returns:
-      A keras sequential model representing either the classification
-        or the box regression head depending on `output_filters`.
-    """
-    head = tf.keras.Sequential([tf.keras.Input(shape=[None, None, 256])])
-    kernel_init = tf.initializers.RandomNormal(0.0, 0.01)
-    for _ in range(4):
-        head.add(
-            tf.keras.layers.Conv2D(256, 3, padding="same", kernel_initializer=kernel_init)
-        )
-        head.add(tf.keras.layers.ReLU())
-    head.add(
-        tf.keras.layers.Conv2D(
-            output_filters,
-            3,
-            1,
-            padding="same",
-            kernel_initializer=kernel_init,
-            bias_initializer=bias_init,
-        )
-    )
-    return head
