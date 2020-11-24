de442627d80c7a13f30a125db8a2b2aef2b1533f
Ankur Kunder <a.kunder@tamu.edu>
2020-11-24 14:33:40 -0600

    "Format changes"

--- tests/unit_tests/tasks/image_test.py
+++ tests/unit_tests/tasks/image_test.py
@@ -14,9 +14,10 @@
 
 from unittest import mock
 
+import numpy as np
+
 import autokeras as ak
 from tests import utils
-import numpy as np
 
 
 @mock.patch("autokeras.AutoModel.fit")
@@ -57,7 +58,9 @@ def test_img_seg_fit_call_auto_model_fit(fit, tmp_path):
 
 @mock.patch("autokeras.AutoModel.fit")
 def test_img_obj_det_fit_call_auto_model_fit(fit, tmp_path):
-    auto_model = ak.tasks.image.ImageObjectDetector(directory=tmp_path, seed=utils.SEED)
+    auto_model = ak.tasks.image.ImageObjectDetector(
+        directory=tmp_path, seed=utils.SEED
+    )
     images = utils.generate_data(num_instances=100, shape=(32, 32, 3))
     bboxes = utils.generate_data(num_instances=100, shape=(3, 4))
     class_ids = utils.generate_data(num_instances=100, shape=(3,))
