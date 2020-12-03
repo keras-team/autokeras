f31f8e6442ba6d2fc028f274428f981726b60b37
Ankur Kunder <a.kunder@tamu.edu>
2020-12-03 13:16:41 -0600

    "unit test changed"

--- tests/utils.py
+++ tests/utils.py
@@ -127,11 +127,11 @@ def get_func_args(func):
 def get_object_detection_data():
     images = generate_data(num_instances=2, shape=(32, 32, 3))
 
-    bbox_0 = np.random.rand((3, 4))
-    class_id_0 = np.random.rand((3,))
+    bbox_0 = np.random.rand(3, 4)
+    class_id_0 = np.random.rand(3,)
 
-    bbox_1 = np.random.rand((5, 4))
-    class_id_1 = np.random.rand((5,))
+    bbox_1 = np.random.rand(5, 4)
+    class_id_1 = np.random.rand(5,)
 
     labels = np.array([(bbox_0, class_id_0), (bbox_1, class_id_1)], dtype=object)
 
