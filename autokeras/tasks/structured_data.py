0cf82edce6043b0aa22f6c994536eb7bf0ad1f09
Anselm Hahn <Anselm.Hahn@gmail.com>
2020-12-10 10:33:09 +0100

    Complete the update

--- autokeras/tasks/structured_data.py
+++ autokeras/tasks/structured_data.py
@@ -280,7 +280,7 @@ class StructuredDataClassifier(SupervisedStructuredDataPipeline):
         validation_data=None,
         **kwargs
     ):
-        """Search for the best model and hyperparameters for the task.
+        """Search for the best model and hyperparameters for the AutoMode.
 
         # Arguments
             x: String, numpy.ndarray, pandas.DataFrame or tensorflow.Dataset.
