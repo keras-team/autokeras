4d8612c4cc93bbbc9d1fea374faf4cdf869e0c03
Haifeng Jin <haifeng-jin@users.noreply.github.com>
2021-03-09 11:12:01 -0600

    setup codespaces

---
+++ .devcontainer/Dockerfile
@@ -0,0 +1,4 @@
+FROM tensorflow/tensorflow:latest
+RUN apt-get -y update
+RUN apt-get -y install git vim
+COPY setup.sh /setup.sh
--- shell/lint.sh
+++ shell/lint.sh
@@ -1,23 +1,26 @@
 isort --sl -c .
 if ! [ $? -eq 0 ]
 then
-    exit 1
+  echo "Please run \"sh shell/format.sh\" to format the code."
+  exit 1
 fi
 flake8 .
 if ! [ $? -eq 0 ]
 then
-    exit 1
+  echo "Please fix the code style issue."
+  exit 1
 fi
 black --check --line-length 85 .
 if ! [ $? -eq 0 ]
 then
-    exit 1
+  echo "Please run \"sh shell/format.sh\" to format the code."
+  exit 1
 fi
 for i in $(find autokeras tests benchmark -name '*.py') # or whatever other pattern...
 do
   if ! grep -q Copyright $i
   then
-    echo $i
+    echo "Please run \"sh shell/format.sh\" to format the code."
     exit 1
   fi
 done
