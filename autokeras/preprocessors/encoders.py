7d3c4a9298d1087adf801f314ba570b5921c0654
Ankur Kunder <a.kunder@tamu.edu>
2021-03-09 19:12:08 -0600

    removed the functions from the class

--- autokeras/preprocessors/encoders.py
+++ autokeras/preprocessors/encoders.py
@@ -269,134 +269,134 @@ class ObjectDetectionLabelEncoder(preprocessor.Preprocessor):
         # class_ids = tf.stack(class_ids)
         # return images, bboxes, class_ids
         print("NEW CHANGE: ", dataset.element_spec)
-        return dataset.map(ObjectDetectionLabelEncoder.data_transform)
+        return dataset.map(data_transform)
 
-    @staticmethod
-    def data_transform(sample):
-        sample_x = sample[0]
-        sample_y = sample[1]
-        print("input to data_transform: ", tf.shape(sample_x), tf.shape(sample_y[0]), tf.shape(sample_y[1]))
-        image = sample_x
-        bbox = ObjectDetectionLabelEncoder.swap_xy(sample_y[0])  # check this function
-        class_id = tf.cast(sample_y[1], dtype=tf.int32)
+# @staticmethod
+def data_transform(sample):
+    sample_x = sample[0]
+    sample_y = sample[1]
+    print("input to data_transform: ", tf.shape(sample_x), tf.shape(sample_y[0]), tf.shape(sample_y[1]))
+    image = sample_x
+    bbox = swap_xy(sample_y[0])  # check this function
+    class_id = tf.cast(sample_y[1], dtype=tf.int32)
 
-        image, bbox = ObjectDetectionLabelEncoder.random_flip_horizontal(image, bbox)
-        image, image_shape, _ = ObjectDetectionLabelEncoder.resize_and_pad_image(image)
+    image, bbox = random_flip_horizontal(image, bbox)
+    image, image_shape, _ = resize_and_pad_image(image)
 
-        bbox = tf.stack(
-            [
-                bbox[:, 0] * image_shape[1],
-                bbox[:, 1] * image_shape[0],
-                bbox[:, 2] * image_shape[1],
-                bbox[:, 3] * image_shape[0],
-            ],
-            axis=-1,
+    bbox = tf.stack(
+        [
+            bbox[:, 0] * image_shape[1],
+            bbox[:, 1] * image_shape[0],
+            bbox[:, 2] * image_shape[1],
+            bbox[:, 3] * image_shape[0],
+        ],
+        axis=-1,
+    )
+    bbox = convert_to_xywh(bbox)
+    return image, bbox, class_id
+
+# @staticmethod
+def random_flip_horizontal(image, boxes):
+    """Flips image and boxes horizontally with 50% chance
+
+    Arguments:
+      image: A 3-D tensor of shape `(height, width, channels)` representing an
+        image.
+      boxes: A tensor with shape `(num_boxes, 4)` representing bounding boxes,
+        having normalized coordinates.
+
+    Returns:
+      Randomly flipped image and boxes
+    """
+    if tf.random.uniform(()) > 0.5:
+        image = tf.image.flip_left_right(image)
+        boxes = tf.stack(
+            [1 - boxes[:, 2], boxes[:, 1], 1 - boxes[:, 0], boxes[:, 3]], axis=-1
         )
-        bbox = ObjectDetectionLabelEncoder.convert_to_xywh(bbox)
-        return image, bbox, class_id
+    return image, boxes
 
-    @staticmethod
-    def random_flip_horizontal(image, boxes):
-        """Flips image and boxes horizontally with 50% chance
+# @staticmethod
+def resize_and_pad_image(
+        image,
+        min_side=800.0,
+        max_side=1333.0,
+        jitter=[640, 1024],
+        stride=128.0,
+):
+    """Resizes and pads image while preserving aspect ratio.
 
-        Arguments:
-          image: A 3-D tensor of shape `(height, width, channels)` representing an
-            image.
-          boxes: A tensor with shape `(num_boxes, 4)` representing bounding boxes,
-            having normalized coordinates.
+    1. Resizes images so that the shorter side is equal to `min_side`
+    2. If the longer side is greater than `max_side`, then resize the image
+      with longer side equal to `max_side`
+    3. Pad with zeros on right and bottom to make the image shape divisible by
+    `stride`
 
-        Returns:
-          Randomly flipped image and boxes
-        """
-        if tf.random.uniform(()) > 0.5:
-            image = tf.image.flip_left_right(image)
-            boxes = tf.stack(
-                [1 - boxes[:, 2], boxes[:, 1], 1 - boxes[:, 0], boxes[:, 3]], axis=-1
-            )
-        return image, boxes
+    Arguments:
+      image: A 3-D tensor of shape `(height, width, channels)` representing an
+        image.
+      min_side: The shorter side of the image is resized to this value, if
+        `jitter` is set to None.
+      max_side: If the longer side of the image exceeds this value after
+        resizing, the image is resized such that the longer side now equals to
+        this value.
+      jitter: A list of floats containing minimum and maximum size for scale
+        jittering. If available, the shorter side of the image will be
+        resized to a random value in this range.
+      stride: The stride of the smallest feature map in the feature pyramid.
+        Can be calculated using `image_size / feature_map_size`.
 
-    @staticmethod
-    def resize_and_pad_image(
-            image,
-            min_side=800.0,
-            max_side=1333.0,
-            jitter=[640, 1024],
-            stride=128.0,
-    ):
-        """Resizes and pads image while preserving aspect ratio.
+    Returns:
+      image: Resized and padded image.
+      image_shape: Shape of the image before padding.
+      ratio: The scaling factor used to resize the image
+    """
+    image_shape = tf.cast(tf.shape(image)[:2], dtype=tf.float32)
+    if jitter is not None:
+        min_side = tf.random.uniform((), jitter[0], jitter[1], dtype=tf.float32)
+    ratio = min_side / tf.reduce_min(image_shape)
+    if ratio * tf.reduce_max(image_shape) > max_side:
+        ratio = max_side / tf.reduce_max(image_shape)
+    image_shape = ratio * image_shape
+    image = tf.image.resize(image, tf.cast(image_shape, dtype=tf.int32))
+    padded_image_shape = tf.cast(
+        tf.math.ceil(image_shape / stride) * stride, dtype=tf.int32
+    )
+    image = tf.image.pad_to_bounding_box(
+        image, 0, 0, padded_image_shape[0], padded_image_shape[1]
+    )
+    return image, image_shape, ratio
 
-        1. Resizes images so that the shorter side is equal to `min_side`
-        2. If the longer side is greater than `max_side`, then resize the image
-          with longer side equal to `max_side`
-        3. Pad with zeros on right and bottom to make the image shape divisible by
-        `stride`
+# @staticmethod
+def swap_xy(boxes):
+    """Swaps order the of x and y coordinates of the boxes.
 
-        Arguments:
-          image: A 3-D tensor of shape `(height, width, channels)` representing an
-            image.
-          min_side: The shorter side of the image is resized to this value, if
-            `jitter` is set to None.
-          max_side: If the longer side of the image exceeds this value after
-            resizing, the image is resized such that the longer side now equals to
-            this value.
-          jitter: A list of floats containing minimum and maximum size for scale
-            jittering. If available, the shorter side of the image will be
-            resized to a random value in this range.
-          stride: The stride of the smallest feature map in the feature pyramid.
-            Can be calculated using `image_size / feature_map_size`.
+    Arguments:
+      boxes: A tensor with shape `(num_boxes, 4)` representing bounding boxes.
 
-        Returns:
-          image: Resized and padded image.
-          image_shape: Shape of the image before padding.
-          ratio: The scaling factor used to resize the image
-        """
-        image_shape = tf.cast(tf.shape(image)[:2], dtype=tf.float32)
-        if jitter is not None:
-            min_side = tf.random.uniform((), jitter[0], jitter[1], dtype=tf.float32)
-        ratio = min_side / tf.reduce_min(image_shape)
-        if ratio * tf.reduce_max(image_shape) > max_side:
-            ratio = max_side / tf.reduce_max(image_shape)
-        image_shape = ratio * image_shape
-        image = tf.image.resize(image, tf.cast(image_shape, dtype=tf.int32))
-        padded_image_shape = tf.cast(
-            tf.math.ceil(image_shape / stride) * stride, dtype=tf.int32
-        )
-        image = tf.image.pad_to_bounding_box(
-            image, 0, 0, padded_image_shape[0], padded_image_shape[1]
-        )
-        return image, image_shape, ratio
+    Returns:
+      swapped boxes with shape same as that of boxes.
+    """
+    print("input to swapy_xy: ", tf.shape(boxes[0]))
+    return tf.stack(
+        [boxes[:, 1], boxes[:, 0], boxes[:, 3], boxes[:, 2]], axis=-1
+    )
 
-    @staticmethod
-    def swap_xy(boxes):
-        """Swaps order the of x and y coordinates of the boxes.
+# @staticmethod
+def convert_to_xywh(boxes):
+    """Changes the box format to center, width and height.
 
-        Arguments:
-          boxes: A tensor with shape `(num_boxes, 4)` representing bounding boxes.
+    Arguments:
+      boxes: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
+        representing bounding boxes where each box is of the format
+        `[xmin, ymin, xmax, ymax]`.
 
-        Returns:
-          swapped boxes with shape same as that of boxes.
-        """
-        print("input to swapy_xy: ", tf.shape(boxes[0]))
-        return tf.stack(
-            [boxes[:, 1], boxes[:, 0], boxes[:, 3], boxes[:, 2]], axis=-1
-        )
-
-    @staticmethod
-    def convert_to_xywh(boxes):
-        """Changes the box format to center, width and height.
-
-        Arguments:
-          boxes: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
-            representing bounding boxes where each box is of the format
-            `[xmin, ymin, xmax, ymax]`.
-
-        Returns:
-          converted boxes with shape same as that of boxes.
-        """
-        return tf.concat(
-            [
-                (boxes[..., :2] + boxes[..., 2:]) / 2.0,
-                boxes[..., 2:] - boxes[..., :2],
-            ],
-            axis=-1,
-        )
+    Returns:
+      converted boxes with shape same as that of boxes.
+    """
+    return tf.concat(
+        [
+            (boxes[..., :2] + boxes[..., 2:]) / 2.0,
+            boxes[..., 2:] - boxes[..., :2],
+        ],
+        axis=-1,
+    )
