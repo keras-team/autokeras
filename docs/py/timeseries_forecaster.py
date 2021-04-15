04b0e2880a0b2340851a8d499895a6a066863800
Abraham Gerard Sebastian <abraham.g.sebastian@gmail.com>
2021-04-15 18:52:05 -0500

    Fix to formatting issues #2

--- docs/py/timeseries_forecaster.py
+++ docs/py/timeseries_forecaster.py
@@ -18,38 +18,76 @@ The first step is to prepare your data. Here we use the [UCI Airquality dataset]
 """
 
 dataset = tf.keras.utils.get_file(
-    fname='AirQualityUCI.csv',
+    fname="AirQualityUCI.csv",
     origin="https://archive.ics.uci.edu/ml/machine-learning-databases/00360/"
     "AirQualityUCI.zip",
-    extract=True
+    extract=True,
 )
 
-dataset = pd.read_csv(dataset, sep=';')
+dataset = pd.read_csv(dataset, sep=";")
 dataset = dataset[dataset.columns[:-2]]
 dataset = dataset.dropna()
-dataset = dataset.replace(',', '.', regex=True)
+dataset = dataset.replace(",", ".", regex=True)
 
 val_split = int(len(dataset) * 0.7)
 data_train = dataset[:val_split]
 validation_data = dataset[val_split:]
 
-data_x = data_train[['CO(GT)', 'PT08.S1(CO)', 'NMHC(GT)', 'C6H6(GT)',
-                     'PT08.S2(NMHC)', 'NOx(GT)', 'PT08.S3(NOx)', 'NO2(GT)',
-                     'PT08.S4(NO2)', 'PT08.S5(O3)', 'T', 'RH']].astype('float64')
+data_x = data_train[
+    [
+        "CO(GT)",
+        "PT08.S1(CO)",
+        "NMHC(GT)",
+        "C6H6(GT)",
+        "PT08.S2(NMHC)",
+        "NOx(GT)",
+        "PT08.S3(NOx)",
+        "NO2(GT)",
+        "PT08.S4(NO2)",
+        "PT08.S5(O3)",
+        "T",
+        "RH",
+    ]
+].astype("float64")
 
-data_x_val = validation_data[['CO(GT)', 'PT08.S1(CO)', 'NMHC(GT)', 'C6H6(GT)',
-                              'PT08.S2(NMHC)', 'NOx(GT)', 'PT08.S3(NOx)', 'NO2(GT)',
-                              'PT08.S4(NO2)', 'PT08.S5(O3)', 'T', 'RH']].astype(
-                                  'float64')
+data_x_val = validation_data[
+    [
+        "CO(GT)",
+        "PT08.S1(CO)",
+        "NMHC(GT)",
+        "C6H6(GT)",
+        "PT08.S2(NMHC)",
+        "NOx(GT)",
+        "PT08.S3(NOx)",
+        "NO2(GT)",
+        "PT08.S4(NO2)",
+        "PT08.S5(O3)",
+        "T",
+        "RH",
+    ]
+].astype("float64")
 
 # Data with train data and the unseen data from subsequent time steps.
-data_x_test = dataset[['CO(GT)', 'PT08.S1(CO)', 'NMHC(GT)', 'C6H6(GT)', 
-                       'PT08.S2(NMHC)', 'NOx(GT)', 'PT08.S3(NOx)', 'NO2(GT)',
-                       'PT08.S4(NO2)', 'PT08.S5(O3)', 'T', 'RH']].astype('float64')
+data_x_test = dataset[
+    [
+        "CO(GT)",
+        "PT08.S1(CO)",
+        "NMHC(GT)",
+        "C6H6(GT)",
+        "PT08.S2(NMHC)",
+        "NOx(GT)",
+        "PT08.S3(NOx)",
+        "NO2(GT)",
+        "PT08.S4(NO2)",
+        "PT08.S5(O3)",
+        "T",
+        "RH",
+    ]
+].astype("float64")
 
-data_y = data_train['AH'].astype('float64')
+data_y = data_train["AH"].astype("float64")
 
-data_y_val = validation_data['AH'].astype('float64')
+data_y_val = validation_data["AH"].astype("float64")
 
 print(data_x.shape)  # (6549, 12)
 print(data_y.shape)  # (6549,)
@@ -68,11 +106,16 @@ clf = ak.TimeseriesForecaster(
     predict_from=predict_from,
     predict_until=predict_until,
     max_trials=1,
-    objective='val_loss'
+    objective="val_loss",
 )
 # Train the TimeSeriesForecaster with train data
-clf.fit(x=data_x, y=data_y, validation_data=(data_x_val, data_y_val), batch_size=32,
-        epochs=10)
+clf.fit(
+    x=data_x,
+    y=data_y,
+    validation_data=(data_x_val, data_y_val),
+    batch_size=32,
+    epochs=10,
+)
 # Predict with the best model(includes original training data).
 predictions = clf.predict(data_x_test)
 print(predictions.shape)
