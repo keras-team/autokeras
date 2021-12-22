c9383a0321341e1311e80e344fba3a52d0425cc5
Haifeng Jin <jhfjhfj1@gmail.com>
2021-12-22 15:24:41 -0600

    tf2.8.0rc0 (#1657)
    Co-authored-by: Haifeng Jin <haifeng-jin@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     keywords=["AutoML", "Keras"],
     install_requires=[
         "packaging",
+        "tensorflow>=2.8.0rc0",
         "keras-tuner>=1.1.0",
         "scikit-learn",
         "pandas",
