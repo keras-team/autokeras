749a1a22bb06ec599f26147820901d93b22c5184
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2022-08-31 04:04:28 +0000

    add unit test

--- autokeras/blocks/preprocessing_test.py
+++ autokeras/blocks/preprocessing_test.py
@@ -14,6 +14,7 @@
 
 import keras_tuner
 import tensorflow as tf
+from keras_tuner.engine import hyperparameters
 from tensorflow import keras
 from tensorflow import nest
 
@@ -88,11 +89,18 @@ def test_augment_build_with_contrast_factor_return_tensor():
 
 
 def test_augment_deserialize_to_augment():
-    serialized_block = blocks.serialize(blocks.ImageAugmentation())
+    serialized_block = blocks.serialize(
+        blocks.ImageAugmentation(
+            zoom_factor=0.1,
+            contrast_factor=hyperparameters.Float("contrast_factor", 0.1, 0.5),
+        )
+    )
 
     block = blocks.deserialize(serialized_block)
 
     assert isinstance(block, blocks.ImageAugmentation)
+    assert block.zoom_factor == 0.1
+    assert isinstance(block.contrast_factor, hyperparameters.Float)
 
 
 def test_augment_get_config_has_all_attributes():
---
+++ shell/coverage.sh
@@ -0,0 +1,1 @@
+pytest --cov-report xml:cov.xml --cov autokeras $1
