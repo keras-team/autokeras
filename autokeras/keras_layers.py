26a0279260abd277b965127cd023d8c76c96f08c
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2022-12-15 01:13:26 +0000

    remove the custom implementation of adamw

--- autokeras/keras_layers.py
+++ autokeras/keras_layers.py
@@ -14,7 +14,6 @@
 import collections
 import math
 import os
-import re
 import unicodedata
 from typing import List
 
@@ -297,121 +296,6 @@ class BertEncoder(layers.Layer):
         )
         checkpoint = tf.train.Checkpoint(model=self)
         checkpoint.restore(path).assert_consumed()
-
-
-@keras.utils.register_keras_serializable()
-class AdamWeightDecay(keras.optimizers.legacy.Adam):
-    """official.nlp.optimization.AdamWeightDecay"""
-
-    def __init__(
-        self,
-        learning_rate=0.001,
-        beta_1=0.9,
-        beta_2=0.999,
-        epsilon=1e-7,
-        amsgrad=False,
-        weight_decay_rate=0.0,
-        include_in_weight_decay=None,
-        exclude_from_weight_decay=None,
-        name="AdamWeightDecay",
-        **kwargs
-    ):
-        super(AdamWeightDecay, self).__init__(
-            learning_rate, beta_1, beta_2, epsilon, amsgrad, name, **kwargs
-        )
-        self.weight_decay_rate = weight_decay_rate
-        self._include_in_weight_decay = include_in_weight_decay
-        self._exclude_from_weight_decay = exclude_from_weight_decay
-
-    def _prepare_local(self, var_device, var_dtype, apply_state):
-        super(AdamWeightDecay, self)._prepare_local(
-            var_device, var_dtype, apply_state
-        )
-        apply_state[(var_device, var_dtype)]["weight_decay_rate"] = tf.constant(
-            self.weight_decay_rate, name="adam_weight_decay_rate"
-        )
-
-    def _decay_weights_op(self, var, learning_rate, apply_state):
-        do_decay = self._do_use_weight_decay(var.name)
-        if do_decay:
-            return var.assign_sub(
-                learning_rate
-                * var
-                * apply_state[(var.device, var.dtype.base_dtype)][
-                    "weight_decay_rate"
-                ],
-                use_locking=self._use_locking,
-            )
-        return tf.no_op()
-
-    def apply_gradients(
-        self, grads_and_vars, name=None, experimental_aggregate_gradients=True
-    ):
-        grads, tvars = list(zip(*grads_and_vars))
-        if experimental_aggregate_gradients:
-            # when experimental_aggregate_gradients = False, apply_gradients() no
-            # longer implicitly allreduce gradients, users manually allreduce
-            # gradient and passed the allreduced grads_and_vars. For now, the
-            # clip_by_global_norm will be moved to before the explicit allreduce to
-            # keep the math the same as TF 1 and pre TF 2.2 implementation.
-            (grads, _) = tf.clip_by_global_norm(
-                grads, clip_norm=1.0
-            )  # pragma: no cover
-        return super(AdamWeightDecay, self).apply_gradients(
-            zip(grads, tvars),
-            name=name,
-            experimental_aggregate_gradients=experimental_aggregate_gradients,
-        )
-
-    def _get_lr(self, var_device, var_dtype, apply_state):
-        """Retrieves the learning rate with the given state."""
-        if apply_state is None:
-            return self._decayed_lr_t[var_dtype], {}  # pragma: no cover
-
-        apply_state = apply_state or {}
-        coefficients = apply_state.get((var_device, var_dtype))
-        if coefficients is None:
-            coefficients = self._fallback_apply_state(
-                var_device, var_dtype
-            )  # pragma: no cover
-            apply_state[(var_device, var_dtype)] = coefficients  # pragma: no cover
-
-        return coefficients["lr_t"], dict(apply_state=apply_state)
-
-    def _resource_apply_dense(self, grad, var, apply_state=None):
-        lr_t, kwargs = self._get_lr(var.device, var.dtype.base_dtype, apply_state)
-        decay = self._decay_weights_op(var, lr_t, apply_state)
-        with tf.control_dependencies([decay]):
-            return super(AdamWeightDecay, self)._resource_apply_dense(
-                grad, var, **kwargs
-            )
-
-    def _resource_apply_sparse(self, grad, var, indices, apply_state=None):
-        lr_t, kwargs = self._get_lr(
-            var.device, var.dtype.base_dtype, apply_state
-        )  # pragma: no cover
-        decay = self._decay_weights_op(var, lr_t, apply_state)  # pragma: no cover
-        with tf.control_dependencies([decay]):  # pragma: no cover
-            return super(
-                AdamWeightDecay, self  # pragma: no cover
-            )._resource_apply_sparse(grad, var, indices, **kwargs)
-
-    def get_config(self):
-        config = super(AdamWeightDecay, self).get_config()
-        config.update(
-            {
-                "weight_decay_rate": self.weight_decay_rate,
-            }
-        )
-        return config
-
-    def _do_use_weight_decay(self, param_name):
-        """Whether to use L2 weight decay for `param_name`."""
-        if self._exclude_from_weight_decay:
-            for r in self._exclude_from_weight_decay:
-                if re.search(r, param_name) is not None:
-                    return False
-        return True
 
 
 @keras.utils.register_keras_serializable()
