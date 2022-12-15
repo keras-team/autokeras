42c896306e0fd29f5dfa922e34041dc1b4f195fb
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2022-12-14 18:26:31 -0800

    Use built-in adamw (#1814)

--- autokeras/keras_layers_test.py
+++ autokeras/keras_layers_test.py
@@ -129,31 +129,6 @@ def test_transformer_encoder_save_and_load(tmp_path):
     keras.models.load_model(os.path.join(tmp_path, "model"))
 
 
-def test_adam_weight_decay(tmp_path):
-    model = keras.Sequential([keras.layers.Dense(10, input_shape=(10,))])
-    lr_schedule = keras.optimizers.schedules.PolynomialDecay(
-        initial_learning_rate=0.1,
-        decay_steps=100,
-        end_learning_rate=0.0,
-    )
-    lr_schedule = layer_module.WarmUp(
-        initial_learning_rate=0.1,
-        decay_schedule_fn=lr_schedule,
-        warmup_steps=10,
-    )
-    optimizer = layer_module.AdamWeightDecay(
-        learning_rate=lr_schedule,
-        weight_decay_rate=0.01,
-        beta_1=0.9,
-        beta_2=0.999,
-        epsilon=1e-6,
-        exclude_from_weight_decay=["LayerNorm", "layer_norm", "bias"],
-    )
-    model.compile(loss="mse", optimizer=optimizer)
-    model.fit(np.random.rand(100, 10), np.random.rand(100, 10), epochs=2)
-    model.save(os.path.join(tmp_path, "model"))
-
-
 def test_cast_to_float32_return_float32_tensor(tmp_path):
     layer = layer_module.CastToFloat32()
 
