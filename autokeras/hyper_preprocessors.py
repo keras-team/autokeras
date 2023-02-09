7461c6067a040beaa107d593f30ab7de51d8e4ee
Neel Kovelamudi <nkovela@google.com>
2023-02-09 00:27:00 +0000

    Fixed imports

--- autokeras/hyper_preprocessors.py
+++ autokeras/hyper_preprocessors.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from tensorflow import keras
 
 from autokeras import preprocessors
 from autokeras.engine import hyper_preprocessor
